30b90c47fb6447e830e810d1961f285af492887e
jaamong <91050780+jaamong@users.noreply.github.com>
2022-01-16 00:40:35 +0900

    Add Plant-Lookup, Select API read function
    모든화분조회(상점), 회원이선택한화분조회 API에서 사용할 함수 생성

---
+++ src/main/java/com/umc/btos/src/plant/PlantProvider.java
@@ -0,0 +1,48 @@
+package com.umc.btos.src.plant;
+
+import com.umc.btos.config.BaseException;
+import com.umc.btos.config.BaseResponse;
+import com.umc.btos.src.plant.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.umc.btos.config.BaseResponseStatus.DATABASE_ERROR;
+
+//Read
+@Service
+public class PlantProvider {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final PlantDao plantDao;
+
+    @Autowired
+    public PlantProvider(PlantDao plantDao){
+        this.plantDao = plantDao;
+    }
+
+
+    //모든식물조회(상점) API
+    public List<GetPlantRes> getAllPlant(int userIdx) throws BaseException {
+        try{
+            return plantDao.getAllPlant(userIdx);
+        }catch(Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    //회원이 선택한 화분 조회 API
+    public GetPlantRes getSelectedPlant(int plantIdx, int userIdx) throws BaseException {
+        try {
+            return plantDao.getSelectedPlant(plantIdx, userIdx);
+        } catch(Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+}
+
