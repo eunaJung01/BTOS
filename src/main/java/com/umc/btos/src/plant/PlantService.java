82e16b6a037c1933a6e118545a638affa930ad1a
jaamong <91050780+jaamong@users.noreply.github.com>
2022-01-16 00:44:25 +0900

    Add Plant-Select API update function
    화분선택 API에서 사용할 함수 생성

---
+++ src/main/java/com/umc/btos/src/plant/PlantService.java
@@ -0,0 +1,36 @@
+package com.umc.btos.src.plant;
+
+import com.umc.btos.config.BaseException;
+import com.umc.btos.config.BaseResponseStatus;
+import com.umc.btos.src.plant.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static com.umc.btos.config.BaseResponseStatus.*;
+
+@Service
+public class PlantService {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final PlantDao plantDao;
+
+    @Autowired
+    public PlantService(PlantDao plantDao){
+        this.plantDao = plantDao;
+    }
+
+    //화분 선택 API
+    public BaseResponseStatus selectPlant(int uPlantIdx) throws BaseException {
+        try{
+            if(plantDao.selectPlant(uPlantIdx) == 1) //변경 성공시
+                return SUCCESS;
+            else //변경 실패시
+                throw new BaseException(MODIFY_FAIL_STATUS);
+        } catch(Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+}
