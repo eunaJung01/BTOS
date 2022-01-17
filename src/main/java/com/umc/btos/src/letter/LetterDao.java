648497c276323aebfdae32b09945bbe6d7e315a2
dumi33 <dumi3345@gmail.com>
2022-01-17 23:41:20 +0900

    Feat LetterDao.java - Get API - check letter
    편지 조회-GET API에 이용되는 Dao java파일

--- src/main/java/com/umc/btos/src/letter/LetterDao.java
+++ src/main/java/com/umc/btos/src/letter/LetterDao.java
@@ -29,4 +29,17 @@ public class LetterDao {
         String lastInsertIdQuery = "select last_insert_id()"; // 가장 마지막에 삽입된(생성된) id값은 가져온다.
         return this.jdbcTemplate.queryForObject(lastInsertIdQuery, int.class); // 해당 쿼리문의 결과 마지막으로 삽인된 유저의 userIdx번호를 반환한다.
     }
+
+    // 해당 letterIdx를 갖는 편지조회
+    public GetLetterRes getLetter(int letterIdx) {
+        String getLetterQuery = "select * from Letter where letterIdx = ?"; // 해당 letterIdx를 만족하는 편지를 조회하는 쿼리문
+        int getLetterParams = letterIdx;
+        return this.jdbcTemplate.queryForObject(getLetterQuery,
+                (rs, rowNum) -> new GetLetterRes(
+                        rs.getInt("letterIdx"),
+                        rs.getInt("replier"),
+                        rs.getInt("receiver"),
+                        rs.getString("content")), // RowMapper(위의 링크 참조): 원하는 결과값 형태로 받기
+                getLetterParams); // 한 개의 편지정보를 얻기 위한 jdbcTemplate 함수(Query, 객체 매핑 정보, Params)의 결과 반환
+    }
 }
