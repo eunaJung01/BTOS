4fb5fbb864faf4f355a9d7ac3f96a1edb64cdccd
jaamong <91050780+jaamong@users.noreply.github.com>
2022-01-17 17:30:18 +0900

    Fix Plant-Response Model's name [before]GetSelectedPlantRes -> [After]GetSpecificPlantRes

--- src/main/java/com/umc/btos/src/plant/model/GetSpecificPlantRes.java
+++ src/main/java/com/umc/btos/src/plant/model/GetSpecificPlantRes.java
@@ -9,12 +9,13 @@ import lombok.Setter;
 @AllArgsConstructor
 
 //Profile에서 회원이 선택한 화분을 조회했을 때를 위한 클래스
-public class GetSelectedPlantRes {
+public class GetSpecificPlantRes {
+    //uPlantIdx가 필요할까? 고민
     private int plantIdx; //화분 식별자
     private String plantName; //화분 이름
     private int maxLevel; //보유 중인 화분의 최대 단계 (Plant.maxLevel)
     private String plantImgUrl; //화분 이미지
-    private int currentLevel; //현재 화분 단계 (UserPlanList.level)
+    private int currentLevel; //현재 화분 단계 (UserPlanList.level) , maxLevel이랑 동일하면 (MAX) 표기
     private String userStatus; //유저의 화분 보유 상태
     private int selectedPlantIdx; //보유중인 화분중 선택된 화분
 }
