c07e6e34ad3c118f37bad1774c6af45faba07ce1
euna <christinejung10@daum.net>
2022-01-17 09:21:56 +0900

    Fix PostDiaryRes

--- src/main/java/com/umc/btos/src/diary/model/PostDiaryRes.java
+++ src/main/java/com/umc/btos/src/diary/model/PostDiaryRes.java
@@ -1,2 +1,15 @@
-package com.umc.btos.src.diary.model;public class PostDiaryRes {
+package com.umc.btos.src.diary.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostDiaryRes {
+    private int diaryIdx;
+    private List doneIdxList;
 }
