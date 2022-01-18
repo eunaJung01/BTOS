956640b8063e5f8603452a0ce1ba284c3cc56c81
euna <christinejung10@daum.net>
2022-01-19 08:43:11 +0900

    Fix Diary-Get Calendar API : GetCalendarRes

--- src/main/java/com/umc/btos/src/diary/model/GetCalendarRes.java
+++ src/main/java/com/umc/btos/src/diary/model/GetCalendarRes.java
@@ -11,4 +11,9 @@ public class GetCalendarRes {
     private String diaryDate; // YYYY-MM-DD
     private int doneListNum; // 하루에 저장되어 있는 done list 개수
     private int emotionIdx;
+
+    public GetCalendarRes(String diaryDate) {
+        this.diaryDate = diaryDate;
+    }
+
 }
