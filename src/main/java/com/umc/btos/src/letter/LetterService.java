f1502863b182cd94974094b92dd237bc6ceb8cbb
dumi33 <dumi3345@gmail.com>
2022-01-18 14:25:18 +0900

    Add PatchLetterReq.java - Patch API - delete letter

--- src/main/java/com/umc/btos/src/letter/LetterService.java
+++ src/main/java/com/umc/btos/src/letter/LetterService.java
@@ -9,8 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.umc.btos.config.BaseResponseStatus.DATABASE_ERROR;
+import static com.umc.btos.config.BaseResponseStatus.MODIFY_FAIL_USERNAME;
 
 @Service
 public class LetterService {
@@ -44,7 +46,17 @@ public class LetterService {
         }
     }
 
-
+    // 편지 삭제 - status를 deleted로 변경 (Patch)
+    public void modifyLetterStaus(PatchLetterReq patchLetterReq) throws BaseException {
+        try {
+            int result = letterDao.modifyLetterStaus(patchProductReq); // 해당 과정이 무사히 수행되면 True(1), 그렇지 않으면 False(0)입니다.
+            if (result == 0) { // result값이 0이면 과정이 실패한 것이므로 에러 메서지를 보냅니다.
+                throw new BaseException(MODIFY_FAIL_LETTER_STATUS);
+            }
+        } catch (Exception exception) { // DB에 이상이 있는 경우 에러 메시지를 보냅니다.
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
 
 
 
