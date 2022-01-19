dba7d65e239f55b8bc7cac46a0d12a27c7116a37
jaamong <91050780+jaamong@users.noreply.github.com>
2022-01-20 00:03:12 +0900

    Change Plant-Request Model
    <PatchSelectPlantReq>
    화분 선택 요청 시 body 형태 변경
    - currentPlant 필드 제거
    - userIdx, futurePlant 필드만 필요

--- src/main/java/com/umc/btos/src/plant/model/PatchSelectPlantReq.java
+++ src/main/java/com/umc/btos/src/plant/model/PatchSelectPlantReq.java
@@ -10,6 +10,6 @@ import lombok.Setter;
 
 //화분 선택 시 Body
 public class PatchSelectPlantReq {
-    private int currentPlant; // 기존의 선택된 화분(status: selected -> active)
+    private int userIdx;
     private int futurePlant; // 사용자가 바꾸기로 선택한 화분(status: active -> selected)
 }
