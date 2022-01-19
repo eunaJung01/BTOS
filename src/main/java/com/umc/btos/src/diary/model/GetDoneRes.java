97cb8114419db6b04193a1b955fa714752100956
euna <christinejung10@daum.net>
2022-01-19 16:45:23 +0900

    Create Diary-Get DiaryList API : Res & Req
    GetDiaryRes & GetDoneRes 클래스 생성

---
+++ src/main/java/com/umc/btos/src/diary/model/GetDoneRes.java
@@ -0,0 +1,14 @@
+package com.umc.btos.src.diary.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetDoneRes {
+    private int doneIdx;
+    private int diaryIdx;
+    private String content;
+}
