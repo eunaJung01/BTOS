3febf32a0b4e0eff5881dc936bd046e11977a26c
dumi33 <dumi3345@gmail.com>
2022-01-20 14:11:16 +0900

    Add Blocklist-Update API add function
    <modifyBlockStatus> block의 status를 deleted로 변경하는 함수

--- src/main/java/com/umc/btos/src/blocklist/BlocklistService.java
+++ src/main/java/com/umc/btos/src/blocklist/BlocklistService.java
@@ -2,6 +2,7 @@ package com.umc.btos.src.blocklist;
 import com.umc.btos.config.BaseException;
 import com.umc.btos.src.blocklist.model.*;
 
+import com.umc.btos.src.letter.model.PatchLetterReq;
 import com.umc.btos.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static com.umc.btos.config.BaseResponseStatus.DATABASE_ERROR;
+import static com.umc.btos.config.BaseResponseStatus.MODIFY_FAIL_BLOCK_STATUS;
 
 
 @Service
@@ -39,6 +41,17 @@ public class BlocklistService {
 
         } catch (Exception exception) { // DB에 이상이 있는 경우 에러 메시지를 보냅니다.
 
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+    // 차단 해제 - status를 deleted로 변경 (Patch)
+    public void modifyBlockStatus(PatchBlocklistReq patchBlocklistReq) throws BaseException {
+        try {
+            int result = blocklistDao.modifyBlockStatus(patchBlocklistReq); // 해당 과정이 무사히 수행되면 True(1), 그렇지 않으면 False(0)입니다.
+            if (result == 0) { // result값이 0이면 과정이 실패한 것이므로 에러 메서지를 보냅니다.
+                throw new BaseException(MODIFY_FAIL_BLOCK_STATUS);
+            }
+        } catch (Exception exception) { // DB에 이상이 있는 경우 에러 메시지를 보냅니다.
             throw new BaseException(DATABASE_ERROR);
         }
     }
