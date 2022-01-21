b19df3a49dc505b40404bb3ec4295496bbf8a017
jaamong <91050780+jaamong@users.noreply.github.com>
2022-01-21 14:20:58 +0900

    Add Shop-Update API new (Dao)
    <withdrawPremium> 청약철회 API
    UPDATE SET 쿼리문 실행
    - User.isPremium : premium -> free
    - 성공하면 1 반환

--- src/main/java/com/umc/btos/src/shop/ShopDao.java
+++ src/main/java/com/umc/btos/src/shop/ShopDao.java
@@ -16,10 +16,18 @@ public class ShopDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    ////프리미엄 계정으로 변경 API
+    //프리미엄 계정으로 변경 API
     public int joinPremium(int userIdx) {
         String Query = "UPDATE User SET isPremium = ? WHERE userIdx =?";
         Object[] Params = new Object[]{"premium", userIdx};
+
+        return this.jdbcTemplate.update(Query, Params);
+    }
+
+    //청약철회 API
+    public int withdrawPremium(int userIdx) {
+        String Query = "UPDATE User SET isPremium = ? WHERE userIdx = ?";
+        Object[] Params = new Object[]{"free", userIdx};
 
         return this.jdbcTemplate.update(Query, Params);
     }
