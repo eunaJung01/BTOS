01752e7f3a108a0bb8422d888dd5d95838826956
gitseon <kisunjjang@naver.com>
2022-01-21 18:03:23 +0900

    ADD UserService checkStatusOfUser

--- src/main/java/com/umc/btos/src/user/UserService.java
+++ src/main/java/com/umc/btos/src/user/UserService.java
@@ -33,7 +33,18 @@ public class UserService {
     // 회원가입(POST)
     public PostUserRes createUser(PostUserReq postUserReq) throws BaseException{
 
-        if (userProvider.checkEmail(postUserReq.getEmail()) == 1) { // 이메일 중복 확인
+        // 회원 상태 확인 : 소셜 로그인 버튼 클릭 회원 정보 가져왔음 (소셜 로그인 api)
+        // 만약 status가 dormant라면 휴면 상태 계정입니다. 재활성화 하시겠습니까? 버튼 누르면 회원 상태 변경 api(재활성화) 호출하도록 만들기
+        // 휴면 아니면 신규 or 탈퇴 회원 -> 회원가입 api
+        // deleted인 경우, 신규인 경우 : 다시 회원가입 진행(새로운 레코드 생성) -> 회원가입 api 호출
+
+
+        if (userProvider.checkStatusOfUser(postUserReq.getEmail()) == "dormant") { // 휴면일 경우 메시지 출력
+            throw new BaseException(POST_USERS_IS_DORMANT);
+        }
+        
+        // status : deleted or 신규회원일 경우 중복 체크
+        if (userProvider.checkEmail(postUserReq.getEmail()) == 1) { // 이메일 중복 확인 -> active인 유저들만 조회
             throw new BaseException(POST_USERS_EXISTS_EMAIL);
         }
 
