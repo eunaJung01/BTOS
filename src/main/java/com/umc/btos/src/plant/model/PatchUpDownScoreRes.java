1a0fae50843eb73779ed149362ad4b0426081bbd
jaamong <91050780+jaamong@users.noreply.github.com>
2022-01-22 15:56:52 +0900

    Add Plant-Response Model
    <PatchUpDownScoreRes>
    점수 증가 또는 레벨 증가 성공 시 response 형태
    - int upScoreOrLevel

---
+++ src/main/java/com/umc/btos/src/plant/model/PatchUpDownScoreRes.java
@@ -0,0 +1,21 @@
+package com.umc.btos.src.plant.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+/*
+점수 증가 또는 레벨 증가 성공 시
+
+<반환값>
+점수 증가 시 : 1
+레벨 증가 시 : 2
+ */
+
+public class PatchUpDownScoreRes {
+    private int upScoreOrLevel;
+}
