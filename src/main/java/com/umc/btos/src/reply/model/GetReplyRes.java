3cd99ef2c3b1e292a5ce17ed7769241a9efa30dc
dumi33 <dumi3345@gmail.com>
2022-01-23 13:48:07 +0900

    Fix Reply-Get API : Res
    변수명 변경
    (변경 전)  replier, receiver
    (변경 후)  replierIdx, receiverIdx

--- src/main/java/com/umc/btos/src/reply/model/GetReplyRes.java
+++ src/main/java/com/umc/btos/src/reply/model/GetReplyRes.java
@@ -10,8 +10,8 @@ import lombok.Setter;
 @AllArgsConstructor // 해당 클래스의 모든 멤버 변수를 받는 생성자를 생성
 public class GetReplyRes {
     private int replyIdx;
-    private int replier;
-    private int receiver;
+    private int replierIdx;
+    private int receiverIdx;
     private String firstType;
     private String content;
 
