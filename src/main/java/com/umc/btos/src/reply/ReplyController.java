a772df71c093180e86ec3c4377fb2df78ee8a232
dumi33 <dumi3345@gmail.com>
2022-01-23 13:49:23 +0900

    Add Reply-POST API : Controller
    POST를 위한 함수를 controller에 추가

--- src/main/java/com/umc/btos/src/reply/ReplyController.java
+++ src/main/java/com/umc/btos/src/reply/ReplyController.java
@@ -2,12 +2,14 @@ package com.umc.btos.src.reply;
 
 
 
+import com.umc.btos.config.BaseException;
+import com.umc.btos.config.BaseResponse;
+import com.umc.btos.src.reply.model.*;
 import com.umc.btos.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/btos/replies")
@@ -25,5 +27,23 @@ public class ReplyController {
         this.replyService = replyService;
         this.jwtService = jwtService;
     }
+    /**
+     * 답장 작성 API
+     * [POST] /btos/replies
+     */
+    // Body
+    @ResponseBody
+    @PostMapping("")    // POST 방식의 요청을 매핑하기 위한 어노테이션
+    public BaseResponse<PostReplyRes> createReply(@RequestBody PostReplyReq postReplyReq) {
+        //  @RequestBody란, 클라이언트가 전송하는 HTTP Request Body(우리는 JSON으로 통신하니, 이 경우 body는 JSON)를 자바 객체로 매핑시켜주는 어노테이션
+        try{
+            PostReplyRes postReplyRes = replyService.createReply(postReplyReq);
+            return new BaseResponse<>(postReplyRes);
+        } catch (BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+
+    }
+
 
 }
