8154d10509b2576283a65a79178c4a1ba1963826
dumi33 <dumi3345@gmail.com>
2022-01-23 14:23:23 +0900

    Add Reply-GET API : Provider

--- src/main/java/com/umc/btos/src/reply/ReplyProvider.java
+++ src/main/java/com/umc/btos/src/reply/ReplyProvider.java
@@ -1,11 +1,16 @@
 package com.umc.btos.src.reply;
 
-import com.umc.btos.src.letter.LetterDao;
+import com.umc.btos.config.BaseException;
+
+
+import com.umc.btos.src.reply.model.*;
 import com.umc.btos.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.umc.btos.config.BaseResponseStatus.DATABASE_ERROR;
 
 @Service
 public class ReplyProvider {
@@ -19,5 +24,13 @@ public class ReplyProvider {
         this.replyDao = replyDao;
         this.jwtService = jwtService;
     }
-
+    // 해당 letterIdx를 갖는 Letter의 정보 조회
+    public GetReplyRes getReply(int replyIdx) throws BaseException {
+        try {
+            GetReplyRes getReplyRes = replyDao.getReply(replyIdx);
+            return getReplyRes;
+        } catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
 }
