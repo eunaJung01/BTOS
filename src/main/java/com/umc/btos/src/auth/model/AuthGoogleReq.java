7c03887d7867a8c5085c40df8bd26a1d6fda5c19
gitseon <kisunjjang@naver.com>
2022-01-23 19:21:40 +0900

    Change Auth AuthGoogleReq member
    email로 변경

--- src/main/java/com/umc/btos/src/auth/model/AuthGoogleReq.java
+++ src/main/java/com/umc/btos/src/auth/model/AuthGoogleReq.java
@@ -8,21 +8,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 
-//승인 요청 시 사용
-//구글에게 요청 매개변수 모델
+// 소셜 로그인 시 받을 body 값
 public class AuthGoogleReq {
-    //요청 필수 매개변수
-    private String redirectUri;
-    private String clientId;
-    private String responseType;
-    private String scope;
-    //구글 애플리케이션 설정 시 선택한 범위, 스코프에 따라 받을 수 있는 유저 정보가 다름
-    private String code;
-
-    private String clientSecret;
-    private String accessType;
-    private String state;
-    private String includeGrantedScopes;
-    private String loginHint;
-    private String prompt;
+    private String email;
 }
