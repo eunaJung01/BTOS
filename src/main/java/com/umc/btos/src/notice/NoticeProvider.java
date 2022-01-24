4c2949cf714a33ce815840e0ebfdfcc1ea1e786b
dumi33 <dumi3345@gmail.com>
2022-01-25 00:40:38 +0900

    Add Notice-GET API : Provider
    <getNotices> 모든 공지들을 조회

--- src/main/java/com/umc/btos/src/notice/NoticeProvider.java
+++ src/main/java/com/umc/btos/src/notice/NoticeProvider.java
@@ -1,10 +1,16 @@
 package com.umc.btos.src.notice;
 
+import com.umc.btos.config.BaseException;
 import com.umc.btos.src.blocklist.BlocklistDao;
+import com.umc.btos.src.notice.model.GetNoticeRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.umc.btos.config.BaseResponseStatus.NOTICE_DATABASE_ERROR;
 
 @Service
 public class NoticeProvider {
@@ -19,4 +25,13 @@ public class NoticeProvider {
         this.noticeDao = noticeDao;
     }
 
+    // 공지들 조회
+    public List<GetNoticeRes> getNotices() throws BaseException {
+        try {
+            List<GetNoticeRes> getNoticeRes = noticeDao.getNotices();
+            return getNoticeRes;
+        } catch (Exception exception) { // 에러가 발생하였다면 : 8006 : 공지 조회 실패
+            throw new BaseException(NOTICE_DATABASE_ERROR);
+        }
+    }
 }
