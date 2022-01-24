23dfd2c837f7698b37659b2f4147c01110fbbc6c
euna <christinejung10@daum.net>
2022-01-24 13:09:21 +0900

    Rename file
    src/test/java/com/umc/btos/Coupang01zApplicationTests.java -> src/test/java/com/umc/btos/BTOSApplicationTests.java

--- src/test/java/com/umc/btos/BTOSApplicationTests.java
+++ src/test/java/com/umc/btos/BTOSApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Coupang01zApplicationTests {
+class BTOSApplicationTests {
 
     @Test
     void contextLoads() {
