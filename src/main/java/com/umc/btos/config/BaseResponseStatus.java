ca5e63307e5f418452896f08f4d8e3a8a76a9788
gitseon <kisunjjang@naver.com>
2022-01-25 00:20:04 +0900

    Change PATCH_USERS_NOT_VALUES msg
    변경하실 정보 -> 변경하실 닉네임으로 변경

--- src/main/java/com/umc/btos/config/BaseResponseStatus.java
+++ src/main/java/com/umc/btos/config/BaseResponseStatus.java
@@ -58,7 +58,7 @@ public enum BaseResponseStatus {
     // 5000 : 도기
     TEMP1(false, 9000, "conflict 방지용 1"),
     POST_USERS_EMPTY_NICKNAME(false, 5000, "닉네임을 입력해주세요."),
-    POST_USERS_INVALID_NICKNAME(false, 5001, "닉네임은 10자까지 입력 가능합니다."),
+    USERS_INVALID_NICKNAME(false, 5001, "닉네임은 10자까지 입력 가능합니다."),
     POST_USERS_DORMANT(false, 5002, "휴면 상태 계정입니다."),
     AUTH_REQ_SIGNUP(false, 5003, "회원가입이 필요합니다."),
     AUTH_FAILED_TO_LOGIN(false, 5004, "로그인에 실패하였습니다."),
@@ -67,7 +67,7 @@ public enum BaseResponseStatus {
     INVALID_USER_STATUS(false, 5007, "탈퇴, 활성화, 휴면 상태값만 가능합니다."),
     MODIFY_FAIL_INFO(false, 5008, "정보 변경에 실패하였습니다."),
     PATCH_USERS_EXISTS_NICKNAME(false, 5009, "이미 존재하는 닉네임입니다."),
-    PATCH_USERS_NOT_VALUES(false, 5010, "변경하실 정보를 입력해주세요."),
+    PATCH_USERS_NOT_VALUES(false, 5010, "변경하실 닉네임을 입력해주세요."),
 
 
     // 6000 : 레마
