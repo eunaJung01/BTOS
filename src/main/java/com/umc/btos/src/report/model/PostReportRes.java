21712c9b14da139f8d80aaf819e983eeefa1b90c
dumi33 <dumi3345@gmail.com>
2022-01-24 20:54:11 +0900

    Fix Report - API : comment

--- src/main/java/com/umc/btos/src/report/model/PostReportRes.java
+++ src/main/java/com/umc/btos/src/report/model/PostReportRes.java
@@ -3,9 +3,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter // 해당 클래스에 대한 접근자 생성
-@Setter // 해당 클래스에 대한 설정자 생성
-@AllArgsConstructor // 해당 클래스의 모든 멤버 변수를 받는 생성자를 생성-
+@Getter
+@Setter
+@AllArgsConstructor
 
 public class PostReportRes {
     private int reportIdx;
