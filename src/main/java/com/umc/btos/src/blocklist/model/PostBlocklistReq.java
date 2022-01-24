2961285d5f83f05ae84cda3f785940c236c7cc10
dumi33 <dumi3345@gmail.com>
2022-01-24 20:52:47 +0900

    Fix Blocklist - API : comment

--- src/main/java/com/umc/btos/src/blocklist/model/PostBlocklistReq.java
+++ src/main/java/com/umc/btos/src/blocklist/model/PostBlocklistReq.java
@@ -3,9 +3,9 @@ package com.umc.btos.src.blocklist.model;
 
 import lombok.*;
 
-@Getter // 해당 클래스에 대한 접근자 생성
-@Setter // 해당 클래스에 대한 설정자 생성
-@AllArgsConstructor // 해당 클래스의 모든 멤버를 받는 생성자를 생성
+@Getter
+@Setter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostBlocklistReq {
     private int blockIdx;
