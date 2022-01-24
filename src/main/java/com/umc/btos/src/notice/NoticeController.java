0c7ac9390bfcca69f7aa6202278a1334fbaf5a20
dumi33 <dumi3345@gmail.com>
2022-01-25 00:38:13 +0900

    Add Notice-GET API : Controller
    <getNotices> 모든 공지들을 조회

--- src/main/java/com/umc/btos/src/notice/NoticeController.java
+++ src/main/java/com/umc/btos/src/notice/NoticeController.java
@@ -2,11 +2,18 @@ package com.umc.btos.src.notice;
 
 
 
+import com.umc.btos.config.BaseException;
+import com.umc.btos.config.BaseResponse;
+import com.umc.btos.src.blocklist.model.GetBlocklistRes;
+import com.umc.btos.src.notice.model.GetNoticeRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.umc.btos.config.BaseResponseStatus.GET_FAIL_USERIDX;
 
 @RestController
 @RequestMapping("/btos/notices")
@@ -21,6 +28,25 @@ public class NoticeController {
     public NoticeController(NoticeProvider noticeProvider, NoticeService noticeService) {
         this.noticeProvider = noticeProvider;
         this.noticeService = noticeService;
+    }
+
+    /**
+     * 공지사항 목록 조회 API
+     * [GET] /btos/notices
+     *
+     *
+     */
+    @ResponseBody
+    @GetMapping("")
+    public BaseResponse<List<GetNoticeRes>> getNotices() { // 공지사항 조회 함수
+        try {
+            //조회 성공 시 : List<GetBlocklistRes> 형태로 결과(공지사항 목록) 반환
+            List<GetNoticeRes> getNoticeRes = noticeProvider.getNotices();
+            return new BaseResponse<>(getNoticeRes);
+
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
     }
 
 }
