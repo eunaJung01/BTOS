78c7808e03ef8ad6278ee452ba5edfaac0207356
dumi33 <dumi3345@gmail.com>
2022-01-24 21:27:25 +0900

    Fix BlockList - API : Controller - comment,jwt
    주석 변경, jwt 코드 삭제

--- src/main/java/com/umc/btos/src/blocklist/BlocklistController.java
+++ src/main/java/com/umc/btos/src/blocklist/BlocklistController.java
@@ -21,27 +21,26 @@ import static com.umc.btos.config.BaseResponseStatus.GET_FAIL_USERIDX;
 @RequestMapping("/btos/blocklists")
 public class BlocklistController {
     final Logger logger = LoggerFactory.getLogger(this.getClass()); // Log를 남기기
-    @Autowired  // 객체 생성을 스프링에서 자동으로 생성해주는 역할. 주입하려 하는 객체의 타입이 일치하는 객체를 자동으로 주입한다.
+    @Autowired
     private final BlocklistProvider blocklistProvider;
     @Autowired
     private final BlocklistService blocklistService;
     @Autowired
-    private final JwtService jwtService;
 
-    public BlocklistController(BlocklistProvider blocklistProvider, BlocklistService blocklistService, JwtService jwtService) {
+
+    public BlocklistController(BlocklistProvider blocklistProvider, BlocklistService blocklistService) {
         this.blocklistProvider = blocklistProvider;
         this.blocklistService = blocklistService;
-        this.jwtService = jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
     }
     /**
             * 차단 작성 API
      * [POST] /btos/blocklists
      */
-    // Body
+    // Body에 json 파일을 담아 차단 데이터 생성
     @ResponseBody
-    @PostMapping("")    // POST 방식의 요청을 매핑하기 위한 어노테이션
+    @PostMapping("")    // POST 어노테이션
     public BaseResponse<PostBlocklistRes> createBlocklist(@RequestBody PostBlocklistReq postBlocklistReq) {
-        //  @RequestBody란, 클라이언트가 전송하는 HTTP Request Body(우리는 JSON으로 통신하니, 이 경우 body는 JSON)를 자바 객체로 매핑시켜주는 어노테이션
+
         try{
             PostBlocklistRes postBlocklistRes = blocklistService.createBlocklist(postBlocklistReq);
             return new BaseResponse<>(postBlocklistRes);
@@ -56,7 +55,7 @@ public class BlocklistController {
      */
     @ResponseBody
     @PatchMapping("/{blockIdx}")
-    // Path-variable
+    // Path-variable - blockIdx를 path-variable로 입력받아 차단의 status를 deleted로 변경
     public BaseResponse<String> modifyBlock(@PathVariable("blockIdx") int blockIdx) {
         try {
             PatchBlocklistReq patchBlocklistReq = new PatchBlocklistReq(blockIdx);
