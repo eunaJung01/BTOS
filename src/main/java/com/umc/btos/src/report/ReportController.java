90edf7c9b5463f771e902e64711b482766a96e99
dumi33 <dumi3345@gmail.com>
2022-01-24 21:31:36 +0900

    Fix Report - API : Controller - comment,jwt
    주석 변경, jwt 코드 삭제

--- src/main/java/com/umc/btos/src/report/ReportController.java
+++ src/main/java/com/umc/btos/src/report/ReportController.java
@@ -12,30 +12,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/btos/reports")
 public class ReportController {
-    final Logger logger = LoggerFactory.getLogger(this.getClass()); // Log를 남기기: 일단은 모르고 넘어가셔도 무방합니다.
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired  // 객체 생성을 스프링에서 자동으로 생성해주는 역할. 주입하려 하는 객체의 타입이 일치하는 객체를 자동으로 주입한다.
+    @Autowired
     private final ReportProvider reportProvider;
     @Autowired
     private final ReportService reportService;
-    @Autowired
-    private final JwtService jwtService;
 
-    public ReportController(ReportProvider reportProvider, ReportService reportService, JwtService jwtService) {
+
+    public ReportController(ReportProvider reportProvider, ReportService reportService) {
         this.reportProvider = reportProvider;
         this.reportService = reportService;
-        this.jwtService = jwtService; // JWT부분은 7주차에 다루므로 모르셔도 됩니다!
     }
 
     /**
      * 신고 작성 API
      * [POST] /btos/reports
      */
-    // Body
+    // Body에 json으로 정보를 입력받아 신고 데이터 생성
     @ResponseBody
-    @PostMapping("")    // POST 방식의 요청을 매핑하기 위한 어노테이션
+    @PostMapping("")    // POST 어노테이션
     public BaseResponse<PostReportRes> createReport(@RequestBody PostReportReq postReportReq) {
-        //  @RequestBody란, 클라이언트가 전송하는 HTTP Request Body(우리는 JSON으로 통신하니, 이 경우 body는 JSON)를 자바 객체로 매핑시켜주는 어노테이션
+
         try{
             PostReportRes postReportRes = reportService.createReport(postReportReq);
             return new BaseResponse<>(postReportRes);
