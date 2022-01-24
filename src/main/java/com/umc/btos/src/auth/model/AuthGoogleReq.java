edaa95b4720c965712c6a9a9f2551541315de9f7
gitseon <kisunjjang@naver.com>
2022-01-25 00:19:10 +0900

    Add annotation Auth AuthGoogleReq
    @NoArgsConstructor추가

--- src/main/java/com/umc/btos/src/auth/model/AuthGoogleReq.java
+++ src/main/java/com/umc/btos/src/auth/model/AuthGoogleReq.java
@@ -1,12 +1,11 @@
 package com.umc.btos.src.auth.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 
 // 소셜 로그인 시 받을 body 값
 public class AuthGoogleReq {
