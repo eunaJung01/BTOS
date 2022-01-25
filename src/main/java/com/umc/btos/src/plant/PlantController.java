ab17997f26f0fa3fe8d1dc662686904ad1afcd78
jaamong <91050780+jaamong@users.noreply.github.com>
2022-01-25 16:09:54 +0900

    Fix Plant-3 APIs
    1. 화분목록조회(Profile)API URI 수정
    [before] /list?userIdx
    [after] /:userIdx/list
    
    2. 회원이 선택한 화분 조회 API URI 수정
    [befroe] /uSelected?plantIdx=&userIdx
    [after] ?plantIdx=&userIdx=
    
    3. 화분 보유중 목록 조회 API 주석처리해놨던거 삭제(필요없음)

--- src/main/java/com/umc/btos/src/plant/PlantController.java
+++ src/main/java/com/umc/btos/src/plant/PlantController.java
@@ -20,10 +20,6 @@ public class PlantController {
     private final PlantProvider plantProvider;
     @Autowired
     private final PlantService plantService;
-    /*
-    @Autowired
-    다른 클래스들 provider, service 등
-     */
 
     public PlantController(PlantProvider plantProvider, PlantService plantService) {
         this.plantProvider = plantProvider;
@@ -33,14 +29,14 @@ public class PlantController {
 
     /**
      * 화분목록조회(Profile) API
-     * [GET] /btos/plants/list?userIdx
-     * Query String : userIdx (mandatory: Y)
+     * [GET] /btos/plants/:userIdx/list
+     * Path variable : userIdx (mandatory: Y)
      */
     @ResponseBody
-    @GetMapping("list")
-    public BaseResponse<List<GetPlantRes>> getPlantList(@RequestParam("userIdx") int userIdx) {
+    @GetMapping("{userIdx}/list")
+    public BaseResponse<List<GetPlantRes>> getPlantList(@PathVariable("userIdx") int userIdx) {
         try {
-            //조회 성공 시 : List<GetPlantRes> 형태로 결과(화분목록) 반환
+            //조회 성공 시 : List<GetPlantRes> 형태로 결과(화분목록) 반환 - 1000
             //DATABASE_ERROR : "데이터베이스 연결에 실패하였습니다." - 4000
             List<GetPlantRes> getPlantRes = plantProvider.getAllPlant(userIdx); //조회(read) -> Provider
             return new BaseResponse<>(getPlantRes);
@@ -51,19 +47,15 @@ public class PlantController {
 
     /**
      * 회원이 선택한 화분 조회 API
-     * 보류 : 더보기 기능 추가시 다시 진행
-     * "미보유 화분"도 더보기 기능이 가능하다면 userIdx안 받고 plantIdx만 받으면 됨
-     * 1. Controller, Provider, Dao 인자 수정
-     * 2. Dao 쿼리 문 수정 -> WHERE Plant.status="active" AND Plant.plnatIdx=입력한 화분 식별자(plantIdx)
-     * 3. 만약 조회한 화분이 회원이 보유중이라면..? 이경우까지.. 음 그러니까 이렇게까지 정보를 출력해야한다면 userIdx 받아야함
-     * [GET] /btos/plants/uSelected?plantIdx=&userIdx=
+     * [GET] /btos/plants?plantIdx=&userIdx=
      * Query String : plantIdx, userIdx (mandatory: Y)
      */
     @ResponseBody
-    @GetMapping("uSelected")
+    @GetMapping("")
     public BaseResponse<GetSpecificPlantRes> getSelectedPlant(@RequestParam("plantIdx") int plantIdx,
                                                               @RequestParam("userIdx") int userIdx) {
         try {
+            //조회 성공 시 : GetSpecificPlantRes 형태로 결과 반환 - 1000
             //DATABASE_ERROR : "데이터베이스 연결에 실패하였습니다." - 4000
             return new BaseResponse<>(plantProvider.getSelectedPlant(plantIdx, userIdx));
         } catch (BaseException exception) {
@@ -129,20 +121,4 @@ public class PlantController {
         }
     }
 
-
-    /*
-     * 화분 보유중 목록 조회 API
-     * [GET] /btos/plant/own?userIdx=
-     * Query String : userIdx (mandatory: Y)
-
-    @ResponseBody
-    @GetMapping("own")
-    public BaseResponse<List<GetSpecificPlantRes>> getOwnPlantList(@RequestParam("userIdx") int userIdx) {
-        try {
-            return new BaseResponse<>(plantProvider.getOwnPlantList(userIdx));
-        } catch (BaseException exception) {
-            return new BaseResponse<>((exception.getStatus()));
-        }
-    }
-    */
 }
