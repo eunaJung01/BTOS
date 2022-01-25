a98609908a51fc0f7b450c2e087f14e61b4e31c7
euna <christinejung10@daum.net>
2022-01-26 05:49:03 +0900

    Fix GetMailboxRes

--- src/main/java/com/umc/btos/src/mailbox/model/GetMailboxRes.java
+++ src/main/java/com/umc/btos/src/mailbox/model/GetMailboxRes.java
@@ -1,4 +1,13 @@
 package com.umc.btos.src.mailbox.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GetMailboxRes {
+    private String type; // diary : 일기 / letter : 편지 / reply : 답장
+    private int idx; // 식별자 (diary - diaryIdx / letter - letterIdx / reply - replyIdx)
+    private String senderName; // 발신자 이름
+    private boolean hasSealing; // 일기 : 0 (실링 X) / 편지, 답장 : 1 (실링 O)
 }
