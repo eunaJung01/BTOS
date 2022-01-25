8952e18e474df2b6f55f6f4818e7139a789511b4
euna <christinejung10@daum.net>
2022-01-25 16:15:52 +0900

    Fix PagingRes
    int startPage = 1;
    생성자 수정 : PagingRes(int currentPage, int dataPerPage);

--- src/main/java/com/umc/btos/config/PagingRes.java
+++ src/main/java/com/umc/btos/config/PagingRes.java
@@ -7,13 +7,14 @@ import lombok.Setter;
 @Setter
 public class PagingRes {
     private boolean hasNext;
-    private int startPage;
+    private int startPage = 1;
     private int endPage;
     private int currentPage;
     private int dataPerPage;
 
-    public PagingRes(int currentPage) {
+    public PagingRes(int currentPage, int dataPerPage) {
         this.currentPage = currentPage;
+        this.dataPerPage = dataPerPage;
     }
 
 }
