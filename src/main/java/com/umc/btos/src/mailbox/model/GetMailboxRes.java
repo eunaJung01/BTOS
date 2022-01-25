61cf1221039116f9290efa88cb0d232bc650bcff
euna <christinejung10@daum.net>
2022-01-26 07:08:04 +0900

    Fix GetMailboxRes
    필드명 변경 : senderName -> senderNickName
    필드 추가 : String sendDate (발신일 = Diary.createAt)
    의미 변경 : boolean hasSealing (일기 : true - 실링 O / 편지, 답장 : false - 실링 X)

--- src/main/java/com/umc/btos/src/mailbox/model/GetMailboxRes.java
+++ src/main/java/com/umc/btos/src/mailbox/model/GetMailboxRes.java
@@ -1,13 +1,16 @@
 package com.umc.btos.src.mailbox.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class GetMailboxRes {
     private String type; // diary : 일기 / letter : 편지 / reply : 답장
     private int idx; // 식별자 (diary - diaryIdx / letter - letterIdx / reply - replyIdx)
-    private String senderName; // 발신자 이름
-    private boolean hasSealing; // 일기 : 0 (실링 X) / 편지, 답장 : 1 (실링 O)
+    private String senderNickName; // 발신자 이름
+    private String sendDate; // 발신일
+    private boolean hasSealing; // 일기 : true (실링 O) / 편지, 답장 : false (실링 X)
 }
