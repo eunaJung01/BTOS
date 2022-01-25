6c46508551be97475d9f79bbddc5577c219ad947
euna <christinejung10@daum.net>
2022-01-25 17:42:25 +0900

    Fix PagingRes

--- src/main/java/com/umc/btos/config/PagingRes.java
+++ src/main/java/com/umc/btos/config/PagingRes.java
@@ -7,9 +7,9 @@ import lombok.Setter;
 @Setter
 public class PagingRes {
     private boolean hasNext;
+    private int currentPage;
     private int startPage = 1;
     private int endPage;
-    private int currentPage;
     private int dataPerPage;
 
     public PagingRes(int currentPage, int dataPerPage) {
