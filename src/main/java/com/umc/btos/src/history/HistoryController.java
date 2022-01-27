6d2d9d6b404b22276db368eb99e86ecf99af130e
euna <christinejung10@daum.net>
2022-01-27 11:05:45 +0900

    Fix HistoryController

--- src/main/java/com/umc/btos/src/history/HistoryController.java
+++ src/main/java/com/umc/btos/src/history/HistoryController.java
@@ -1,11 +1,19 @@
 package com.umc.btos.src.history;
 
+import com.umc.btos.config.BaseResponsePaging;
+import com.umc.btos.src.history.model.GetHistoryListRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/histories")
 public class HistoryController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -15,5 +23,21 @@ public class HistoryController {
     public HistoryController(HistoryProvider historyProvider) {
         this.historyProvider = historyProvider;
     }
+
+    /*
+     * History 조회
+     * [GET] /histories/list/:userIdx/:pageNum?filtering=&search=
+     * filtering = 1. receiver : 발신인 / 2. diary : 일기만 / 3. letter : 편지만
+     * search = 검색할 문자열 ("String")
+     * 최신순 정렬 (1. receiver : diaryDate & createdAt 기준 / 2. diary : diaryDate 기준 / 3. letter : createdAt 기준 -> 내림차순 정렬)
+     * 페이징 처리 (무한 스크롤) - 20개씩 조회
+     */
+//    @ResponseBody
+//    @GetMapping("/list/{userIdx}/{pageNum}")
+//    BaseResponsePaging<List<GetHistoryListRes>> getHistoryList() {
+//        try {
+//
+//        }
+//    }
 
 }
