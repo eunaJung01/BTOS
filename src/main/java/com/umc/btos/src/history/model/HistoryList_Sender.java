5d38de646a6c2aac86e81d3cbe984dee0b6fbef7
euna <christinejung10@daum.net>
2022-01-29 12:14:15 +0900

    Fix HistoryList_Sender (Res)

--- src/main/java/com/umc/btos/src/history/model/HistoryList_Sender.java
+++ src/main/java/com/umc/btos/src/history/model/HistoryList_Sender.java
@@ -12,8 +12,9 @@ public class HistoryList_Sender {
     private int historyListNum; // 개수
     private List<History> historyList; // 내용
 
-    public HistoryList_Sender(String senderNickName) {
+    public HistoryList_Sender(String senderNickName, List<History> historyList) {
         this.senderNickName = senderNickName;
+        this.historyList = historyList;
     }
 
 }
