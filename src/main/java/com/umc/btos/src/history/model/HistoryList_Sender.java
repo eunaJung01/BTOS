7e1e85b16528124a3494809ed8530f8cf7ba682c
euna <christinejung10@daum.net>
2022-01-29 17:53:41 +0900

    Add HistoryList_Sender @AllArgsConstructor

--- src/main/java/com/umc/btos/src/history/model/HistoryList_Sender.java
+++ src/main/java/com/umc/btos/src/history/model/HistoryList_Sender.java
@@ -1,5 +1,6 @@
 package com.umc.btos.src.history.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class HistoryList_Sender {
     private String senderNickName; // 발신자 이름
     private int historyListNum; // 개수
