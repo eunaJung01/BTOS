17420c79164392543bf8084de68d351eaa9a74f9
euna <christinejung10@daum.net>
2022-01-29 12:15:50 +0900

    Add History-Get History List API : filtering
    filtering = "sender"일 경우 response 구현 완료

--- src/main/java/com/umc/btos/src/history/HistoryDao.java
+++ src/main/java/com/umc/btos/src/history/HistoryDao.java
@@ -1,10 +1,12 @@
 package com.umc.btos.src.history;
 
+import com.umc.btos.src.history.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class HistoryDao {
@@ -13,6 +15,84 @@ public class HistoryDao {
     @Autowired
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    // 일기 & 편지 발신자 닉네임 목록 반환 (createdAt 기준 내림차순 정렬)
+    public List<String> getNickNameList_sortedByCreatedAt(int userIdx) {
+        String query = "SELECT DISTINCT senderNickName FROM (" +
+                "SELECT User.nickName AS senderNickName, Diary.createdAt AS sendAt FROM User " +
+                "INNER JOIN (DiarySendList INNER JOIN Diary ON DiarySendList.diaryIdx = Diary.diaryIdx) ON User.userIdx = Diary.userIdx " +
+                "WHERE DiarySendList.receiverIdx = ? " +
+                "UNION " +
+                "SELECT User.nickName AS senderNickName, Letter.createdAt AS sendAt FROM User " +
+                "INNER JOIN (LetterSendList INNER JOIN Letter ON LetterSendList.letterIdx = Letter.letterIdx) ON User.userIdx = Letter.userIdx " +
+                "WHERE LetterSendList.receiverIdx = ? " +
+                "ORDER BY sendAt DESC" +
+                ") senderNickName";
+
+        return this.jdbcTemplate.queryForList(query, String.class, userIdx, userIdx);
+    }
+
+    // null 확인 : Diary
+    public int hasHistory_diary(int userIdx, String senderNickName) {
+        String query = "SELECT COUNT(*) " +
+                "FROM DiarySendList " +
+                "INNER JOIN Diary ON DiarySendList.diaryIdx = Diary.diaryIdx " +
+                "INNER JOIN User ON Diary.userIdx = User.userIdx " +
+                "WHERE DiarySendList.receiverIdx = ? AND User.nickName = ? ";
+
+        return this.jdbcTemplate.queryForObject(query, int.class, userIdx, senderNickName);
+    }
+
+    // null 확인 : Letter
+    public int hasHistory_letter(int userIdx, String senderNickName) {
+        String query = "SELECT COUNT(*) " +
+                "FROM LetterSendList " +
+                "INNER JOIN Letter ON LetterSendList.letterIdx = Letter.letterIdx " +
+                "INNER JOIN User ON Letter.userIdx = User.userIdx " +
+                "WHERE LetterSendList.receiverIdx = ? AND User.nickName = ? ";
+
+        return this.jdbcTemplate.queryForObject(query, int.class, userIdx, senderNickName);
+    }
+
+    // 일기 리스트 반환 (DiarySendList.receiverIdx = userIdx AND User.nickName = senderNickName)
+    public List<History> getDiaryList(int userIdx, String senderNickName) {
+        String query = "SELECT Diary.diaryIdx AS idx, User.nickName AS senderNickName," +
+                "Diary.content AS diaryContent, Diary.emotionIdx AS emotionIdx, COUNT(Done.diaryIdx) AS doneListNum, Diary.createdAt AS sendAt " +
+                "FROM DiarySendList " +
+                "INNER JOIN Diary ON DiarySendList.diaryIdx = Diary.diaryIdx " +
+                "INNER JOIN User ON Diary.userIdx = User.userIdx " +
+                "INNER JOIN Done ON Diary.diaryIdx = Done.diaryIdx " +
+                "WHERE DiarySendList.receiverIdx = ? AND User.nickName = ? " +
+                "ORDER BY sendAt DESC";
+
+        return this.jdbcTemplate.query(query,
+                (rs, rowNum) -> new History(
+                        "diary",
+                        rs.getInt("idx"),
+                        rs.getString("senderNickName"),
+                        new History_Diary(rs.getString("diaryContent"), rs.getInt("emotionIdx"), rs.getInt("doneListNum")),
+                        rs.getString("sendAt")
+                ), userIdx, senderNickName);
+    }
+
+    // 편지 리스트 반환 (LetterSendList.receiverIdx = userIdx AND User.nickName = senderNickName)
+    public List<History> getLetterList(int userIdx, String senderNickName) {
+        String query = "SELECT Letter.letterIdx AS idx, User.nickName AS senderNickName, Letter.content AS content, Letter.createdAt AS sendAt " +
+                "FROM LetterSendList " +
+                "INNER JOIN Letter ON LetterSendList.letterIdx = Letter.letterIdx " +
+                "INNER JOIN User ON Letter.userIdx = User.userIdx " +
+                "WHERE LetterSendList.receiverIdx = ? AND User.nickName = ? " +
+                "ORDER BY sendAt DESC";
+
+        return this.jdbcTemplate.query(query,
+                (rs, rowNum) -> new History(
+                        "letter",
+                        rs.getInt("idx"),
+                        rs.getString("senderNickName"),
+                        rs.getString("content"),
+                        rs.getString("sendAt")
+                ), userIdx, senderNickName);
     }
 
 }
--- src/main/java/com/umc/btos/src/history/HistoryProvider.java
+++ src/main/java/com/umc/btos/src/history/HistoryProvider.java
@@ -1,9 +1,18 @@
 package com.umc.btos.src.history;
 
+import com.umc.btos.config.BaseException;
+import com.umc.btos.src.history.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+import static com.umc.btos.config.BaseResponseStatus.*;
 
 @Service
 public class HistoryProvider {
@@ -14,6 +23,67 @@ public class HistoryProvider {
     @Autowired
     public HistoryProvider(HistoryDao historyDao) {
         this.historyDao = historyDao;
+    }
+
+    /*
+     * History 목록 조회
+     * [GET] /histories/list/:userIdx/:pageNum?filtering=&search=
+     * filtering = 1. sender : 발신인 / 2. diary : 일기만 / 3. letter : 편지만
+     * search = 검색할 문자열 ("String")
+     * 최신순 정렬 (createdAt 기준 내림차순 정렬)
+     * 페이징 처리 (무한 스크롤) - 20개씩 조회
+     */
+    public GetHistoryListRes getHistoryList(String[] params) throws BaseException {
+        try {
+            int userIdx = Integer.parseInt(params[0]);
+            String filtering = params[1];
+            String search = params[2];
+
+            GetHistoryListRes historyListRes = new GetHistoryListRes(filtering);
+
+            if (filtering.compareTo("sender") == 0) {
+                // userIdx 회원이 받은 일기와 편지의 발신자 닉네임 목록 (createdAt 기준 내림차순 정렬)
+                List<String> senderNickNameList = historyDao.getNickNameList_sortedByCreatedAt(userIdx);
+
+                List<HistoryList_Sender> historyListRes_list = new ArrayList<>(); // GetHistoryListRes.list
+
+                // senderNickNameList에 저장되어 있는 닉네임 순서대로 HistoryList_Sender 객체 생성
+                // -> HistoryList_Sender 객체들을 List 형태로 묶어서 GetHistoryListRes.list에 저장
+                for (String senderNickName : senderNickNameList) {
+                    List<History> historyList = new ArrayList<>(); // HistoryList_Sender.historyList
+                    HistoryList_Sender historyList_sender = new HistoryList_Sender(senderNickName, historyList);
+
+                    if (historyDao.hasHistory_diary(userIdx, senderNickName) != 0) { // null 확인
+                        historyList_sender.getHistoryList().addAll(historyDao.getDiaryList(userIdx, senderNickName)); // 일기
+                    }
+                    if (historyDao.hasHistory_letter(userIdx, senderNickName) != 0) { // null 확인
+                        historyList_sender.getHistoryList().addAll(historyDao.getLetterList(userIdx, senderNickName)); // 편지
+                    }
+                    Collections.sort(historyList_sender.getHistoryList()); // createAt 기준 내림차순 정렬
+
+                    historyList_sender.setHistoryListNum(historyList_sender.getHistoryList().size()); // HistoryList_Sender.historyListNum
+                    historyListRes_list.add(historyList_sender);
+                }
+                historyListRes.setList(historyListRes_list);
+
+            } else if (filtering.compareTo("diary") == 0) {
+
+            } else {
+
+            }
+
+            // diary 내용 복호화
+
+            if (search != null) {
+
+            }
+
+            return historyListRes;
+
+        } catch (Exception exception) {
+            System.out.println(exception);
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 
 }
