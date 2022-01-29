6cebe38a088c2a1ff8c54f5c22c66c6010829033
euna <christinejung10@daum.net>
2022-01-30 01:29:37 +0900

    Fix History Res : HistoryList_Sender

--- src/main/java/com/umc/btos/src/history/model/HistoryList_Sender.java
+++ src/main/java/com/umc/btos/src/history/model/HistoryList_Sender.java
@@ -4,19 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @AllArgsConstructor
 public class HistoryList_Sender {
     private String senderNickName; // 발신자 이름
     private int historyListNum; // 개수
-    private List<History> historyList; // 내용
-
-    public HistoryList_Sender(String senderNickName, List<History> historyList) {
-        this.senderNickName = senderNickName;
-        this.historyList = historyList;
-    }
-
+    private History firstContent; // 내용 (createdAt 기준 내림차순 정렬 시 가장 윗 항목)
 }
