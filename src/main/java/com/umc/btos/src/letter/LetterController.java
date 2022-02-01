3fe04223b4272f3a1cd59663f3e24f5bef715842
dumi33 <dumi3345@gmail.com>
2022-02-01 22:15:30 +0900

    Fix Letter - Post API : Controller
    발송되는 유저의 userIdx배열과 화분점수 증가치 모두를 한번에 반환
    [PostLetterPlantRes 이용]

--- src/main/java/com/umc/btos/src/letter/LetterController.java
+++ src/main/java/com/umc/btos/src/letter/LetterController.java
@@ -12,6 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/letters")
 public class LetterController {
@@ -40,15 +42,17 @@ public class LetterController {
     // Body에 json 파일 형식으로 넣음
     @ResponseBody
     @PostMapping("")    // POST 방식의 요청을 매핑하기 위한 어노테이션
-    public BaseResponse<PatchModifyScoreRes> createLetter(@RequestBody PostLetterReq postLetterReq) {
+    public BaseResponse<PostLetterPlantRes> createLetter(@RequestBody PostLetterReq postLetterReq) {
 
         try{
-            letterService.createLetter(postLetterReq); // letter, letterSendList에 컬럼 추가
+            List<Integer> receiveUserIdx = letterService.createLetter(postLetterReq); // letter, letterSendList에 컬럼 추가
             // 화분 점수 증가
-            PatchModifyScoreRes result = plantService.modifyScore_plus(postLetterReq.getUserIdx(), Constant.PLANT_LEVELUP_LETTER,"letter");
-            return new BaseResponse<>(result);
+            PatchModifyScoreRes ModifyScore = plantService.modifyScore_plus(postLetterReq.getUserIdx(), Constant.PLANT_LEVELUP_LETTER,"letter");
+            PostLetterPlantRes result_all = new PostLetterPlantRes(receiveUserIdx,ModifyScore ); // new 다음에 대문자여야한다.
+
+            return new BaseResponse<>(result_all);
         } catch (BaseException exception){
-            return new BaseResponse<>((exception.getStatus()));
+            return new BaseResponse<>(exception.getStatus());
         }
 
     }
