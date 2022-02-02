b76aeea6b1ef212c6ee20e18746e4f50125d5f81
dumi33 <dumi3345@gmail.com>
2022-02-02 12:43:21 +0900

    Fix Report - POST API : Controller
    신고당한 유저userIdx와 화분점수를 동시에 반환
    [PostReportUserIdxPlantRes를 반환]

--- src/main/java/com/umc/btos/src/report/ReportController.java
+++ src/main/java/com/umc/btos/src/report/ReportController.java
@@ -3,6 +3,7 @@ package com.umc.btos.src.report;
 import com.umc.btos.config.BaseException;
 import com.umc.btos.config.BaseResponse;
 import com.umc.btos.config.Constant;
+import com.umc.btos.src.letter.model.PostLetterPlantRes;
 import com.umc.btos.src.plant.PlantService;
 import com.umc.btos.src.plant.model.PatchModifyScoreRes;
 import com.umc.btos.src.report.model.*;
@@ -11,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import static com.umc.btos.config.BaseResponseStatus.POST_REPORT_REASON;
 
 @RestController
 @RequestMapping("/reports")
@@ -38,23 +41,29 @@ public class ReportController {
     // Body에 json으로 정보를 입력받아 신고 데이터 생성
     @ResponseBody
     @PostMapping("")    // POST 어노테이션
-    public BaseResponse<String> createReport(@RequestBody PostReportReq postReportReq) {
+    public BaseResponse<PostReportUserIdxPlantRes> createReport(@RequestBody PostReportReq postReportReq) {
         try{
             String ReportReason = postReportReq.getReason(); // 신고 사유 // spam : 스팸 / sex : 성적 / hate : 혐오 / dislike : 마음에 안듦 / etc : 기타
             reportService.createReport(postReportReq);
              if ((ReportReason.equals("sex")) || (ReportReason.equals("hate"))) {
              // 화분 점수 감소 //-100
-                plantService.modifyScore_minus(reportService.getUserIdx(postReportReq), Constant.PLANT_LEVELDOWN_REPORT_SEX_HATE, "report_sex_hate");
+                 PatchModifyScoreRes ModifyScore_sex_hate = plantService.modifyScore_minus(reportService.getUserIdx(postReportReq), Constant.PLANT_LEVELDOWN_REPORT_SEX_HATE, "report_sex_hate");
+                 PostReportUserIdxPlantRes result_sex_hate = new PostReportUserIdxPlantRes(reportService.getUserIdx(postReportReq), ModifyScore_sex_hate );
+                 return new BaseResponse<>(result_sex_hate);
              } else if ((ReportReason.equals("spam")) || (ReportReason.equals("dislike"))) {
              // 화분 점수 감소 // -30
-                plantService.modifyScore_minus(reportService.getUserIdx(postReportReq), Constant.PLANT_LEVELDOWN_REPORT_SPAM_DISLIKE, "report_spam_dislike");
+                 PatchModifyScoreRes ModifyScore_spam_dislike = plantService.modifyScore_minus(reportService.getUserIdx(postReportReq), Constant.PLANT_LEVELDOWN_REPORT_SPAM_DISLIKE, "report_spam_dislike");
+                 PostReportUserIdxPlantRes result_spam_dislike = new PostReportUserIdxPlantRes(reportService.getUserIdx(postReportReq), ModifyScore_spam_dislike );
+                 return new BaseResponse<>(result_spam_dislike);
              }
              else if ((ReportReason.equals("etc"))) {
-             String result = "신고-(기타)가 완료되었습니다.";
-             return new BaseResponse<>(result);
+                 PatchModifyScoreRes ModifyScore_null = new PatchModifyScoreRes(false,"report_etc");
+                 PostReportUserIdxPlantRes result_etc = new PostReportUserIdxPlantRes(reportService.getUserIdx(postReportReq), ModifyScore_null );
+                 return new BaseResponse<>(result_etc);
              }
-            String result = "신고가 완료되었습니다.";
-            return new BaseResponse<>(result);
+             else {
+                 throw new BaseException(POST_REPORT_REASON);
+             }
         } catch (BaseException exception){
 
             return new BaseResponse<>((exception.getStatus()));
