b18a472ec7aaa27fdb9b2f1e0036d0e1de99a97b
dumi33 <dumi3345@gmail.com>
2022-02-03 23:45:30 +0900

    Fix Reply - POST API : Req - comment
    변수 sendIdx 추가, 주석 추가
    firstType -> firstHistoryType으로 변수명 변경

--- src/main/java/com/umc/btos/src/reply/model/PostReplyReq.java
+++ src/main/java/com/umc/btos/src/reply/model/PostReplyReq.java
@@ -11,8 +11,9 @@ public class PostReplyReq {
     private int replyIdx;
     private int replierIdx;
     private int receiverIdx;
-    private int isChecked;
-    private String firstType;
+    private int isChecked; // default = 0
+    private String firstHistoryType; // 시작점 구분 // diary : 일기, letter : 편지
+    private int sendIdx; // DiarySendList.sendIdx, LetterSendList.sendIdx
     private String content;
 
 
