05500159d936118d93c19456c401170455dc1e69
euna <christinejung10@daum.net>
2022-02-05 01:47:02 +0900

    Clean codes in LetterProvider

--- src/main/java/com/umc/btos/src/letter/LetterProvider.java
+++ src/main/java/com/umc/btos/src/letter/LetterProvider.java
@@ -26,21 +26,17 @@ public class LetterProvider {
     }
 
     // 해당 letterIdx를 갖는 Letter 조회
-    public GetLetterRes getLetter(int letterIdx,int userIdx) throws BaseException {
-        try{// 열람여부 변경 성공 여부 반환
-            int isSuccess = letterDao.modifyIsChecked(letterIdx,userIdx);// 성공 시 1, 실패 시 0을 반환
-            if (isSuccess==0){
+    public GetLetterRes getLetter(int userIdx, int letterIdx) throws BaseException {
+        try {
+            GetLetterRes getLetterRes = letterDao.getLetter(letterIdx, userIdx);
+
+            int isSuccess = letterDao.modifyIsChecked(letterIdx, userIdx); // 열람여부 변경 성공 여부 반환 : 성공 시 1, 실패 시 0을 반환
+            if (isSuccess == 0) {
                 throw new BaseException(MODIFY_LETTERSENDLIST_ISCHECKED_ERROR);
             }
-        }catch (Exception exception) {
-            throw new BaseException(DATABASE_ERROR);
-        }
-        try {
-            GetLetterRes getLetterRes = letterDao.getLetter(letterIdx,userIdx);
-
             return getLetterRes;
-        } catch (Exception exception) {
 
+        } catch (Exception exception) {
             throw new BaseException(DATABASE_ERROR);
         }
     }
