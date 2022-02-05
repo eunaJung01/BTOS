a6acba81a3c92a151802a0018c5d459bf83d3809
정은아 <86337233+eunaJung01@users.noreply.github.com>
2022-02-06 04:13:51 +0900

    Test for Send Diary
    EC2 서버 일기 발송 알고리즘 실행 시간 테스트

--- src/main/java/com/umc/btos/src/diary/DiaryProvider.java
+++ src/main/java/com/umc/btos/src/diary/DiaryProvider.java
@@ -92,10 +92,11 @@ public class DiaryProvider {
 
     // Diary - Send Algorithm
 //     @Scheduled(cron = "55 59 18 * * *") // 매일 18:59:55에 DiarySendList 생성
-    @Scheduled(cron = "00 50 22 * * *") // test
+    @Scheduled(cron = "00 16 04 * * *") // test
     public void sendDiary() {
 
         LocalDate yesterday = LocalDate.now().minusDays(1); // 어제 날짜 (yyyy-MM-dd)
+        System.out.println(yesterday);
         List<Integer> diaryIdxList = diaryDao.getDiaryIdxList(yesterday.toString()); // 당일 발송해야 하는 모든 diaryIdx
 //        List<Integer> diaryIdxList = diaryDao.getDiaryIdxList("2022-02-01"); // test
 
