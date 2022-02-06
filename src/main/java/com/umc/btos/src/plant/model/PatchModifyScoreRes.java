ff6be6916746c20abb900c89b9a7b6618f9bfe79
euna <christinejung10@daum.net>
2022-02-06 18:05:47 +0900

    Fix Plant - PatchModifyScoreRes
    필드명 변경 : boolean isLevelChanged -> boolean levelChanged (boolean은 isPrefix 인식 문제 존재)

--- src/main/java/com/umc/btos/src/plant/model/PatchModifyScoreRes.java
+++ src/main/java/com/umc/btos/src/plant/model/PatchModifyScoreRes.java
@@ -10,11 +10,11 @@ import lombok.Setter;
 public class PatchModifyScoreRes {
     private String type; // 1. diary / 2. letter / 3. report_sex_hate / 4. report_spam_dislike / 5.report_etc
     private String status = null; // 단계 증가 : levelUp / 단계 감소 : levelDown / 최대 단계 & 최대 점수인 경우라 변동 없을 때 : null
-    private boolean isLevelChanged; // 단계 변경되었으면 true, 아니면 false
+    private boolean levelChanged; // 단계 변경되었으면 true, 아니면 false
 
-    public PatchModifyScoreRes(boolean isLevelChanged, String type) {
+    public PatchModifyScoreRes(boolean levelChanged, String type) {
         this.type = type;
-        this.isLevelChanged = isLevelChanged;
+        this.levelChanged = levelChanged;
     }
 
 }
