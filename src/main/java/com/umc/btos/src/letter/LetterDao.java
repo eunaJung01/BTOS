b235124d66bd533549d301e6d60ae4fc90b7d9b6
dumi33 <dumi3345@gmail.com>
2022-02-06 17:01:38 +0900

    Add Letter - POST API : Dao
    로직을 Service로 옮기면서 메서드들을 분해

--- src/main/java/com/umc/btos/src/letter/LetterDao.java
+++ src/main/java/com/umc/btos/src/letter/LetterDao.java
@@ -29,8 +29,8 @@ public class LetterDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    // =================================== 편지 생성 ===================================
 
-    // 편지 생성
     public int createLetter(PostLetterReq postLetterReq) { // 생성한 letterIdx 반환
         String createLetterQuery = "insert into Letter (userIdx,content) VALUES (?,?)"; // 실행될 동적 쿼리문
         Object[] createLetterParams = new Object[]{postLetterReq.getUserIdx(), postLetterReq.getContent()}; // 동적 쿼리의 ?부분에 주입될 값
@@ -41,46 +41,42 @@ public class LetterDao {
         return this.jdbcTemplate.queryForObject(lastInsertIdQuery, int.class); // 해당 쿼리문의 결과 마지막으로 삽인된 유저의 letterIdx번호를 반환한다.
     }
 
-    // 편지 발송 // letterSendList에 isSad상태가 아닌 userIdx 5개를 골라 SendList에 컬럼 추가하는 함수
-    public List<Integer> createLetterSendList(int letterIdx) { // 생성된 letter의 Idx를 인수로 받는다.
-        //해당 편지를 발송한 userIdx
+    public PostLetterUserSimilarIdx getIdx_Similar(int letterIdx) { //편지를 전송하는 사람의 userIdx와 similarAge의 여부를 반환
         String getUserIdxQuery = "SELECT L.userIdx FROM Letter L WHERE L.letterIdx=?";
-        int getUserIdxParam = letterIdx;
-        int userIdx = this.jdbcTemplate.queryForObject(getUserIdxQuery, int.class, getUserIdxParam);
+        int userIdx = this.jdbcTemplate.queryForObject(getUserIdxQuery, int.class, letterIdx);
         // 편지 발송 유저의 또래 편지 수신 여부
         String getSimilarAgeQuery = "SELECT U.recSimilarAge FROM User U WHERE userIdx=?";
         int userSimilarAge = this.jdbcTemplate.queryForObject(getSimilarAgeQuery, int.class, userIdx);
+        PostLetterUserSimilarIdx userIdx_SimilarAge = new PostLetterUserSimilarIdx(userIdx, userSimilarAge);
+        return userIdx_SimilarAge;
+    }
 
-        if (userSimilarAge == 1) { // 편지 발송 유저가 또래 편지 수신을 원할경우
+    public List<Integer> getLetterUserIdx_Similar(PostLetterUserSimilarIdx postLetterUserSimilarIdx) { // 편지를 수신할 유저의 userIdx들을 list형태로 반환 // +-5살의 유저만 선택
+        String getBirthQuery = "SELECT U.birth FROM User U WHERE userIdx=?"; // 해당 유저의 생년을 구한다.
+        int getUserIdxParam = postLetterUserSimilarIdx.getUserIdx();
+        int userBirth = this.jdbcTemplate.queryForObject(getBirthQuery, int.class, getUserIdxParam);
 
-            String getBirthQuery = "SELECT U.birth FROM User U WHERE userIdx=?"; // 해당 유저의 생년을 구한다.
-            int userBirth = this.jdbcTemplate.queryForObject(getBirthQuery, int.class, userIdx);
+        String getUserIdx = "select U.userIdx from User U where U.status='active' and U.recOthers = 1 and ( (?-5) <= U.birth and U.birth <=(?+5))  order by rand() limit 5";
+        // 휴먼상태가 아니고, 타인의 편지를 수신하는 유저이고, 나이대가 +-5년의 유저 중
+        // 랜덤으로 5명의 userIdx를 뽑는 쿼리문
+        List<Integer> userIdx_Similar = this.jdbcTemplate.queryForList(getUserIdx, int.class, userBirth, userBirth); // 변수 userIdx에 랜덤으로 뽑은 userIdx를 넣는다.
 
-            String getUserIdx = "select U.userIdx from User U where U.status='active' and U.recOthers = 1 and ( (?-5) <= U.birth and U.birth <=(?+5))  order by rand() limit 5";
-            // 휴먼상태가 아니고, 타인의 편지를 수신하는 유저이고, 나이대가 +-5년의 유저 중
-            // 랜덤으로 5명의 userIdx를 뽑는 쿼리문
-            List<Integer> userIdx_Similar = this.jdbcTemplate.queryForList(getUserIdx, int.class, userBirth, userBirth); // 변수 userIdx에 랜덤으로 뽑은 userIdx를 넣는다.
+        return userIdx_Similar;
+    }
 
-            for (int i = 0; i < 5; i++) { // 5명의 userIdx를 뽑는다. // 1명씩 테이블에 추가하므로 5번 반복
-                String createLetterSendListQuery = "insert into LetterSendList (letterIdx,receiverIdx) VALUES (?,?)"; // 실행될 동적 쿼리문
-                Object[] createLetterSendListParams = new Object[]{letterIdx, userIdx_Similar.get(i)}; // 동적 쿼리의 ?부분에 주입될 값
-                this.jdbcTemplate.update(createLetterSendListQuery, createLetterSendListParams);
-            }
-            return userIdx_Similar;
-        } else { //편지 발송 유저가 또래 편지 수신을 원하지 않을경우
-            String getUserIdx = "select U.userIdx from User U where U.status='active' and recOthers = 1 order by rand() limit 5";
-            // 휴먼상태가 아니고, 타인의 편지를 수신하는 유저 중
-            // 랜덤으로 5명의 userIdx를 뽑는 쿼리문
-            List<Integer> userIdx_unSimilar = this.jdbcTemplate.queryForList(getUserIdx, int.class); // 변수 userIdx에 랜덤으로 뽑은 userIdx를 넣는다.
+    public void createLetterSendList(int letterIdx, List<Integer> userIdx_list,int i){ // LetterSendList 테이블에 추가
+        String createLetterSendListQuery = "insert into LetterSendList (letterIdx,receiverIdx) VALUES (?,?)"; // 실행될 동적 쿼리문
+        Object[] createLetterSendListParams = new Object[]{letterIdx, userIdx_list.get(i)}; // 동적 쿼리의 ?부분에 주입될 값
+        this.jdbcTemplate.update(createLetterSendListQuery, createLetterSendListParams);
+    }
 
-            for (int i = 0; i < 5; i++) { // 5명의 userIdx를 뽑는다. // 1명씩 테이블에 추가하므로 5번 반복
-                String createLetterSendListQuery = "insert into LetterSendList (letterIdx,receiverIdx) VALUES (?,?)"; // 실행될 동적 쿼리문
-                Object[] createLetterSendListParams = new Object[]{letterIdx, userIdx_unSimilar.get(i)}; // 동적 쿼리의 ?부분에 주입될 값
-                this.jdbcTemplate.update(createLetterSendListQuery, createLetterSendListParams);
-            }
-            return userIdx_unSimilar;
-        }
+    public List<Integer> getLetterUserIdx (PostLetterUserSimilarIdx postLetterUserSimilarIdx) { // 편지를 수신할 유저의 userIdx들을 list형태로 반환
+        String getUserIdx = "select U.userIdx from User U where U.status='active' and recOthers = 1 order by rand() limit 5";
+        // 휴먼상태가 아니고, 타인의 편지를 수신하는 유저 중
+        // 랜덤으로 5명의 userIdx를 뽑는 쿼리문
+        List<Integer> userIdx_unSimilar = this.jdbcTemplate.queryForList(getUserIdx, int.class); // 변수 userIdx에 랜덤으로 뽑은 userIdx를 넣는다.
 
+        return userIdx_unSimilar;
     }
 
     //편지 삭제 // 해당 letterIdx의 편지 status를 deleted로 변경
