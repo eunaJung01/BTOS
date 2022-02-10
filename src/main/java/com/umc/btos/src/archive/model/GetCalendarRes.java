8b4f698d8d89f65e3a5bd5068bccbe03ed684e18
euna <christinejung10@daum.net>
2022-02-10 22:09:46 +0900

    Fix Archive-Get Calendar : response
    GetCalendarRes - int diaryIdx 필드 추가

--- src/main/java/com/umc/btos/src/archive/model/GetCalendarRes.java
+++ src/main/java/com/umc/btos/src/archive/model/GetCalendarRes.java
@@ -6,11 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class GetCalendarRes {
+    private int diaryIdx;
     private String diaryDate; // YYYY.MM.DD
     private int doneListNum; // 하루에 저장되어 있는 done list 개수
     private int emotionIdx;
 
-    public GetCalendarRes(String diaryDate) {
+    public GetCalendarRes(int diaryIdx, String diaryDate) {
+        this.diaryIdx = diaryIdx;
         this.diaryDate = diaryDate;
     }
 
