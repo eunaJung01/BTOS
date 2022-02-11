b894af0f3f55b4b1545c8df75c56a6e48587367b
dumi33 <dumi3345@gmail.com>
2022-02-11 22:44:51 +0900

    Fix Letter - POST API : Service
    createLetter 메서드의 반환타입 변경
    getNickName 메서드 추가

--- src/main/java/com/umc/btos/src/letter/LetterService.java
+++ src/main/java/com/umc/btos/src/letter/LetterService.java
@@ -14,8 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static com.umc.btos.config.BaseResponseStatus.DATABASE_ERROR;
-import static com.umc.btos.config.BaseResponseStatus.MODIFY_FAIL_LETTER_STATUS;
+import static com.umc.btos.config.BaseResponseStatus.*;
 
 
 @Service
@@ -37,34 +36,40 @@ public class LetterService {
     // ******************************************************************************
     // 편지 작성(POST)
 
-    public List<Integer> createLetter(PostLetterReq postLetterReq) throws BaseException {
-
+    public PostLetterRes createLetter(PostLetterReq postLetterReq) throws BaseException {
         try {
             int letterIdx = letterDao.createLetter(postLetterReq); // 편지 생성 //letter 테이블에 생성 // 생성한 편지의 letterIdx반환
             PostLetterUserSimilarIdx idx_similar =letterDao.getIdx_Similar(letterIdx); // 생성한 유저의 idx와 SimilarAge여부를 반환
-
             if (idx_similar.getUserSimilarAge() == 1){// 편지 발송 유저가 또래 편지 수신을 원할경우
-                List<Integer> receiveUserIdx_similar =letterDao.getLetterUserIdx_Similar(idx_similar); // 휴먼상태가 아닌 또래의 user 5명을 랜덤으로 골라 편지 발송 //letterSendList에 추가
+                List<Integer> receiveUserIdx_similar =letterDao.getLetterUserIdx_Similar(idx_similar);
+                    // 휴먼상태가 아닌 또래의 user 5명을 랜덤으로 골라 편지 발송 //letterSendList에 추가
                 for (int i = 0; i < 5; i++) { // 5명의 userIdx를 뽑는다. // 1명씩 테이블에 추가하므로 5번 반복
                     letterDao.createLetterSendList(letterIdx,receiveUserIdx_similar,i);
                 }
-                return receiveUserIdx_similar;
+                PostLetterRes postLetterRes_similar = new PostLetterRes(letterIdx, receiveUserIdx_similar );
+                return postLetterRes_similar;
             }
-
             else {// 편지 발송 유저가 또래 편지 수신을 원하지않는 경우
-                List<Integer> receiveUserIdx =letterDao.getLetterUserIdx(idx_similar); // 휴먼상태가 아닌 user 5명을 랜덤으로 골라 편지 발송 //letterSendList에 추가
-                for (int i = 0; i < 5; i++) { // 5명의 userIdx를 뽑는다. // 1명씩 테이블에 추가하므로 5번 반복
-                    letterDao.createLetterSendList(letterIdx,receiveUserIdx,i);
-                }
-                return receiveUserIdx;
+                    List<Integer> receiveUserIdx =letterDao.getLetterUserIdx(idx_similar); // 휴먼상태가 아닌 user 5명을 랜덤으로 골라 편지 발송 //letterSendList에 추가
+                    for (int i = 0; i < 5; i++) { // 5명의 userIdx를 뽑는다. // 1명씩 테이블에 추가하므로 5번 반복
+                        letterDao.createLetterSendList(letterIdx,receiveUserIdx,i);
+                    }
+                PostLetterRes postLetterRes = new PostLetterRes(letterIdx, receiveUserIdx );
+                    return postLetterRes;
             }
-
         } catch (Exception exception) { // DB에 이상이 있는 경우 에러 메시지를 보냅니다.
             exception.printStackTrace(); // 에러 발생 원인 출력
             throw new BaseException(DATABASE_ERROR);
         }
     }
-
+    public String getNickName(int userIdx) throws BaseException {
+        try {
+            String nickName = letterDao.getNickName(userIdx); // 해당 과정이 무사히 수행되면 True(1), 그렇지 않으면 False(0)입니다.
+            return nickName;
+        } catch (Exception exception) { // DB에 이상이 있는 경우 에러 메시지를 보냅니다.
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
     // 편지 삭제 - status를 deleted로 변경 (Patch)
     public void modifyLetterStatus(PatchLetterReq patchLetterReq) throws BaseException {
         try {
