591bb9bf704aeb96e8d158d191699aebb10620d4
dumi33 <dumi3345@gmail.com>
2022-02-12 19:27:52 +0900

    Fix Letter - POST API : Res
    PostLetterPlantRes의
    receiverIdxList로 컬럼명 변경

--- src/main/java/com/umc/btos/src/letter/model/PostLetterPlantRes.java
+++ src/main/java/com/umc/btos/src/letter/model/PostLetterPlantRes.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class PostLetterPlantRes {
     private int letterIdx;
     private String senderNickName; //편지를 보낸 사람의 닉네임
-    private List<Integer> receiveUserIdx; //전송한 유저idx들
+    private List<Integer> receiverIdxList; //전송한 유저idx들
     private PatchModifyScoreRes patchModifyScoreRes; // 화분점수 변경 반환
 
 }
