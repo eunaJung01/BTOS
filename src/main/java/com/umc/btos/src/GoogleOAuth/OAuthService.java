81118b876abbe66679a51385ddc6655de885bc24
jaamong <91050780+jaamong@users.noreply.github.com>
2022-02-12 20:52:03 +0900

    Add GoogleOAuth-methods (service)
    1. request
    - controller에서 받은 로그인 처리
    - 리디렉션 url 생성
    - 생성된 url로 리디렉션
    
    2. requestAccessToken
    - access token 요청

--- src/main/java/com/umc/btos/src/GoogleOAuth/OAuthService.java
+++ src/main/java/com/umc/btos/src/GoogleOAuth/OAuthService.java
@@ -1,4 +1,38 @@
 package com.umc.btos.src.GoogleOAuth;
 
+import com.umc.btos.src.GoogleOAuth.model.GoogleOAuth;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+
 public class OAuthService {
+    private final GoogleOAuth googleOAuth;
+    private final HttpServletResponse response;
+
+    /**
+     * 구글 로그인 처리
+     */
+    public void request() {
+        String redirectURL = googleOAuth.getOauthRedirectURL();
+
+        try {
+            response.sendRedirect(redirectURL); //토큰 요청을 하도록 리다이렉트
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 로그인 처리 후 토큰 요청
+     * @param code
+     * @return
+     */
+    public String requestAccessToken(String code) {
+        return googleOAuth.requestAccessToken(code);
+    }
 }
