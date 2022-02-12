b9474e98c7a366e10f37b00a315b9edd212c5e2a
dumi33 <dumi3345@gmail.com>
2022-02-13 01:48:34 +0900

    Fix Letter - Post API : Service
    5명보다 작은 userIdxList가 나온다면 랜덤으로 편지 전송하는 로직 추가

--- src/main/java/com/umc/btos/src/letter/LetterService.java
+++ src/main/java/com/umc/btos/src/letter/LetterService.java
@@ -12,9 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.umc.btos.config.BaseResponseStatus.*;
+
 
 
 @Service
@@ -43,8 +46,29 @@ public class LetterService {
             if (idx_similar.getUserSimilarAge() == 1){// 편지 발송 유저가 또래 편지 수신을 원할경우
                 List<Integer> receiveUserIdx_similar =letterDao.getLetterUserIdx_Similar(idx_similar);
                     // 휴먼상태가 아닌 또래의 user 5명을 랜덤으로 골라 편지 발송 //letterSendList에 추가
-                for (int i = 0; i < 5; i++) { // 5명의 userIdx를 뽑는다. // 1명씩 테이블에 추가하므로 5번 반복
+                for (int i = 0; i < receiveUserIdx_similar.size(); i++) { //(최대 5명) 5명의 userIdx를 뽑는다. // 1명씩 테이블에 추가하므로 5번 반복
                     letterDao.createLetterSendList(letterIdx,receiveUserIdx_similar,i);
+                }
+                if (receiveUserIdx_similar.size() < 5 ){ // 또래의 유저가 5명 미만이라면
+                    Map<Integer, Integer> letterIdxMap = new HashMap<>();
+                    for (int pastSend : receiveUserIdx_similar) {
+                        letterIdxMap.put(pastSend, 1); // 이미 편지를 보낸 유저의 값을 1로 지정
+                    }
+                    //  (5 - 선택된 유저) 만큼 랜덤으로 보낼 유저 인덱스의 리스트 만들기
+                    List<Integer> receiveUserIdx_Random =letterDao.getLetterUserIdx_Random(idx_similar);
+                    // 또래 이외에 (5 - 선택된 유저) 만큼 랜덤으로 선택한 유저에게 편지 발송
+                    int sendUserIdx = 0;
+                    int similar_size = receiveUserIdx_similar.size();
+                    for (int j = 0; j < (5-similar_size); j++,sendUserIdx++){ // j 번만큼 편지 전송
+                        int userIdx = receiveUserIdx_Random.get(sendUserIdx);
+                        if (letterIdxMap.containsKey(userIdx)==true) { // 만약 이미 편지를 보낸 유저라면
+                            j -= 1; // 편지를 보낸 수 줄임
+                            continue;
+                        }
+                        letterDao.createLetterSendList(letterIdx,receiveUserIdx_Random,sendUserIdx);
+                        receiveUserIdx_similar.add(userIdx);
+                    }
+
                 }
                 PatchModifyScoreRes ModifyScore = plantService.modifyScore_plus(postLetterReq.getUserIdx(), Constant.PLANT_LEVELUP_LETTER,"letter");
                 String senderNickName = getNickName(postLetterReq.getUserIdx());
@@ -52,8 +76,9 @@ public class LetterService {
                 return result_similar;
             }
             else {// 편지 발송 유저가 또래 편지 수신을 원하지않는 경우
+                // (최대 5명) userIdx를 뽑는다.
                 List<Integer> receiveUserIdx =letterDao.getLetterUserIdx(idx_similar); // 휴먼상태가 아닌 user 5명을 랜덤으로 골라 편지 발송 //letterSendList에 추가
-                for (int i = 0; i < 5; i++) { // 5명의 userIdx를 뽑는다. // 1명씩 테이블에 추가하므로 5번 반복
+                for (int i = 0; i < receiveUserIdx.size(); i++) { // 1명씩 테이블에 추가 // 5명보다 작을경우 더이상 보낼 유저가 없는것이므로 나온 유저의 수 만큼만 편지를 전송한다.
                     letterDao.createLetterSendList(letterIdx,receiveUserIdx,i);
                 }
                 PatchModifyScoreRes ModifyScore = plantService.modifyScore_plus(postLetterReq.getUserIdx(), Constant.PLANT_LEVELUP_LETTER,"letter");
