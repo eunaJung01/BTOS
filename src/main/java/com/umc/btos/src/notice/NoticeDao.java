a8c0cdfa9dd1b02ce7630d0c5b11cafc7a330a50
dumi33 <dumi3345@gmail.com>
2022-02-14 01:29:41 +0900

    Fix Notice - API : Dao - Comment
    주석 추가 & 변경

--- src/main/java/com/umc/btos/src/notice/NoticeDao.java
+++ src/main/java/com/umc/btos/src/notice/NoticeDao.java
@@ -20,7 +20,8 @@ public class NoticeDao {
 
     // Notice 테이블에 존재하는 전체 공지들 조회
     public List<GetNoticeRes> getNotices() {
-        String getNoticesQuery = "select noticeIdx,title,content,date_format(createdAt, '%Y.%m.%d.%H:%i:%s') from Notice"; //공지 조회 쿼리
+        // 생성 시간 Format = "년.월.일.시.분.초"
+        String getNoticesQuery = "select noticeIdx,title,content,date_format(createdAt, '%Y.%m.%d.%H:%i:%s') from Notice";
         return this.jdbcTemplate.query(getNoticesQuery,
                 (rs, rowNum) -> new GetNoticeRes(
                         rs.getInt("noticeIdx"),
