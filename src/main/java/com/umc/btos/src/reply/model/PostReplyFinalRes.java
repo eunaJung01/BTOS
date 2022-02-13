ab8809bacfd88ddff05ecf5a67e01c782337036e
dumi33 <dumi3345@gmail.com>
2022-02-14 01:30:59 +0900

    Fix Reply - POST API : Res - Comment
    주석 추가 & 변경

--- src/main/java/com/umc/btos/src/reply/model/PostReplyFinalRes.java
+++ src/main/java/com/umc/btos/src/reply/model/PostReplyFinalRes.java
@@ -10,5 +10,5 @@ import lombok.Setter;
 public class PostReplyFinalRes {
     private int replyIdx;
     private int receiverIdx; // 편지를 받는 사람의 userIdx
-    private String senderNickName;
+    private String senderNickName; // 편지를 받는 유저의 닉네임
 }
