0594794696149fce00732db1e6a152fbb5e2a53a
euna <christinejung10@daum.net>
2022-02-14 11:04:06 +0900

    Fix ReplyService

--- src/main/java/com/umc/btos/src/reply/ReplyService.java
+++ src/main/java/com/umc/btos/src/reply/ReplyService.java
@@ -5,7 +5,7 @@ import com.umc.btos.config.BaseException;
 
 
 import com.umc.btos.src.alarm.AlarmService;
-import com.umc.btos.src.reply.model.*;\
+import com.umc.btos.src.reply.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
