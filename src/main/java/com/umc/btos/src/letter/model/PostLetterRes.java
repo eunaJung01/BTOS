eb0a3406d8561e6aa3055eb91bec386c43f57893
euna <christinejung10@daum.net>
2022-02-15 21:11:35 +0900

    Fix Letter - PostLetterRes

--- src/main/java/com/umc/btos/src/letter/model/PostLetterRes.java
+++ src/main/java/com/umc/btos/src/letter/model/PostLetterRes.java
@@ -16,4 +16,11 @@ public class PostLetterRes {
     private String senderNickName; // 발신인 닉네임
     private List<Receiver> receiverList; // 수신인 목록
     private PatchModifyScoreRes plantRes; // 편지를 발송 후 해당 회원의 화분에 대한 정보 (점수 및 단계 증가)
+
+    public PostLetterRes(int letterIdx, String senderNickName, List<Receiver> receiverList) {
+        this.letterIdx = letterIdx;
+        this.senderNickName = senderNickName;
+        this.receiverList = receiverList;
+    }
+
 }
