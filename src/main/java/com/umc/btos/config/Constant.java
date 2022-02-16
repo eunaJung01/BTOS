64513d6aaf58db7bcb2938b081a1d6ac6c524a66
euna <christinejung10@daum.net>
2022-02-16 12:47:53 +0900

    Add Constant
    상수 추가
    1. SIMILAR_AGE_STANDARD : 일기 & 편지 - 비슷한 나이대 기준 (-n ~ +n)
    2. DIARY_REC_SIMILAR_AGE_RATIO : 일기 - 비슷한 나이대로 수신할 수 있는 회원의 최대 비율 (%)
    3. LETTER_SEND_TOTAL_NUM : 편지 - 편지를 발송해야 하는 총 횟수
    4. LETTER_SEND_SIMILAR_AGE_NUM : 편지 - 비슷한 나이대로 발송할 최대 횟수

--- src/main/java/com/umc/btos/config/Constant.java
+++ src/main/java/com/umc/btos/config/Constant.java
@@ -15,8 +15,16 @@ public class Constant {
     // 화분 - 단계 별 성장치 [LEVEL_0, LEVEL_1, LEVEL_2, LEVEL_3]
     public static final int[] PLANT_LEVEL = new int[] {15, 30, 50, 50};
 
+    // 일기 & 편지 - 비슷한 나이대 기준 (-n ~ +n)
+    public static final int SIMILAR_AGE_STANDARD = 5;
 
     // 일기 - 비슷한 나이대로 수신할 수 있는 회원의 최대 비율 (%)
     public static final int DIARY_REC_SIMILAR_AGE_RATIO = 80;
+
+    // 편지 - 편지를 발송해야 하는 총 횟수
+    public static final int LETTER_SEND_TOTAL_NUM = 5;
+
+    // 편지 - 비슷한 나이대로 발송할 최대 횟수
+    public static final int LETTER_SEND_SIMILAR_AGE_NUM = 3;
 
 }
