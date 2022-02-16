5c0799d632d8db16cd24e9a6f223a93741942021
euna <christinejung10@daum.net>
2022-02-17 08:04:07 +0900

    Add Letter - Get Letter : validation

--- src/main/java/com/umc/btos/src/letter/LetterController.java
+++ src/main/java/com/umc/btos/src/letter/LetterController.java
@@ -93,6 +93,17 @@ public class LetterController {
     @GetMapping("/{letterIdx}")
     public BaseResponse<GetLetterRes> getLetter(@PathVariable("letterIdx") int letterIdx, @RequestParam("userIdx") int userIdx) {
         try {
+            // TODO : 형식적 validation - 존재하는 회원인가? / 존재하는 편지인가? / 해당 회원이 작성한 편지인가?
+            if (letterProvider.checkUserIdx(userIdx) == 0) {
+                throw new BaseException(INVALID_USERIDX); // 존재하지 않는 회원입니다.
+            }
+            if (letterProvider.checkLetterIdx(letterIdx) == 0) {
+                throw new BaseException(INVALID_LETTERIDX); // 존재하지 않는 편지입니다.
+            }
+            if (letterProvider.checkUserAboutLetter(userIdx, letterIdx) == 0) {
+                throw new BaseException(INVALID_USER_ABOUT_LETTER); // 해당 편지에 접근 권한이 없는 회원입니다.
+            }
+
             GetLetterRes getLetterRes = letterProvider.getLetter(letterIdx, userIdx);
             return new BaseResponse<>(getLetterRes);
 
