01495ea49fdc44cbaa01cec24b13a6d98d6447f0
euna <christinejung10@daum.net>
2022-02-21 21:49:34 +0900

    Fix Reply - Post Reply API
    reason = etc일 경우 PlantModifyScoreRes 처리 추가

--- src/main/java/com/umc/btos/src/report/ReportController.java
+++ src/main/java/com/umc/btos/src/report/ReportController.java
@@ -3,6 +3,7 @@ package com.umc.btos.src.report;
 import com.umc.btos.config.BaseException;
 import com.umc.btos.config.BaseResponse;
 import com.umc.btos.config.Constant;
+import com.umc.btos.src.plant.PlantDao;
 import com.umc.btos.src.plant.PlantService;
 import com.umc.btos.src.plant.model.PatchModifyScoreRes;
 import com.umc.btos.src.report.model.*;
@@ -10,8 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import static com.umc.btos.config.BaseResponseStatus.*;
 
 @RestController
 @RequestMapping("/reports")
@@ -24,26 +23,27 @@ public class ReportController {
     private final ReportService reportService;
     @Autowired
     private final PlantService plantService;
+    @Autowired
+    private final PlantDao plantDao;
 
 
-    public ReportController(ReportProvider reportProvider, ReportService reportService, PlantService plantService) {
+    public ReportController(ReportProvider reportProvider, ReportService reportService, PlantService plantService, PlantDao plantDao) {
         this.reportProvider = reportProvider;
         this.reportService = reportService;
         this.plantService = plantService;
+        this.plantDao = plantDao;
     }
 
     /*
      * 일기/편지/답장 신고
      * [POST] /reports
-     * 1. 신고 접수
+     * 1. 신고 저장
      * 2. 화분 점수와 단계 변경
      */
     @ResponseBody
     @PostMapping("")
     public BaseResponse<PostReportRes> postReport(@RequestBody PostReportReq postReportReq) {
         try {
-            // TODO : 형식적 validation - type, typeIdx, reason
-
             // 신고를 당한 회원 식별자
             int reportedUserIdx = reportProvider.getReportedUserIdx(postReportReq.getType(), postReportReq.getTypeIdx());
 
@@ -58,8 +58,13 @@ public class ReportController {
                 postReportRes.setPatchModifyScoreRes(plantService.modifyScore_minus(reportedUserIdx, Constant.PLANT_LEVELDOWN_REPORT_SEX_HATE, "report"));
             }
             // reason = spam, dislike
-            else {
+            else if (reason.equals("spam") || reason.equals("dislike")) {
                 postReportRes.setPatchModifyScoreRes(plantService.modifyScore_minus(reportedUserIdx, Constant.PLANT_LEVELDOWN_REPORT_SPAM_DISLIKE, "report"));
+            }
+            // reason = etc
+            else {
+                int plantLevel = plantDao.getLevel(reportedUserIdx);
+                postReportRes.setPatchModifyScoreRes(new PatchModifyScoreRes("reply", null, false, plantLevel));
             }
 
             return new BaseResponse<>(postReportRes);
