44c99d3ebb172758b69c7d992cef243758a44257
euna <christinejung10@daum.net>
2022-02-21 21:03:54 +0900

    Fix PostReportReq

--- src/main/java/com/umc/btos/src/report/model/PostReportReq.java
+++ src/main/java/com/umc/btos/src/report/model/PostReportReq.java
@@ -5,7 +5,6 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 public class PostReportReq {
-    private int reportIdx;
     private String type; // 신고 구분 (Tables - diary : 일기 / letter : 편지 / reply : 답장)
     private int typeIdx; // 신고 구분 식별자 (PKs - diaryIdx / letterIdx / replyIdx)
     private String reason; // 신고 사유 (spam : 스팸 / sex : 성적 컨텐츠 / hate : 혐오 발언 또는 괴롭힘 / dislike : 마음에 들지 않습니다 / etc : 기타)
