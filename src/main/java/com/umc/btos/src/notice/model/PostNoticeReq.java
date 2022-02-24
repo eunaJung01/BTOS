91ec3efbe6237f6bd27b0ffea1b48c196ef990d1
gitseon <kisunjjang@naver.com>
2022-02-25 01:11:11 +0900

    Refactor notice PostNoticeReq
    NoArgsConstructor annotation 추가

--- src/main/java/com/umc/btos/src/notice/model/PostNoticeReq.java
+++ src/main/java/com/umc/btos/src/notice/model/PostNoticeReq.java
@@ -1,12 +1,11 @@
 package com.umc.btos.src.notice.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostNoticeReq {
     private String title; // 제목
     private String content; // 내용
