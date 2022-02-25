7c7d0168d52ec2ad50db8fd232a9a3654b291e53
gitseon <kisunjjang@naver.com>
2022-02-25 17:40:46 +0900

    Fix plant FcmService
    화분 성장 시 푸시 알림 제대로 가도록 수정

--- src/main/java/com/umc/btos/src/plant/PlantFcmService.java
+++ src/main/java/com/umc/btos/src/plant/PlantFcmService.java
@@ -25,7 +25,7 @@ public class PlantFcmService {
     private final ObjectMapper objectMapper;
     private final String IMAGE_URL = "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f8e3655e-b372-4971-937f-89a5722483f6/app_icon.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220224%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220224T001544Z&X-Amz-Expires=86400&X-Amz-Signature=c3b7bb368be3c4d3c2b754a8dece7156a596a49a5ae9e425a6dfbe6eb63f3d33&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22app%2520icon.png%22&x-id=GetObject";
 
-    public FcmResponse sendMessageTo(String targetToken, String title, String body) throws IOException {
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
         String message = makeMessage(targetToken, title, body);
 
         OkHttpClient client = new OkHttpClient();
@@ -38,18 +38,17 @@ public class PlantFcmService {
                 .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
                 .build();
 
-        /*
         Response response = client.newCall(request)
                 .execute();
-
+        /*
         System.out.println("code " + response.code() +
                 "body : "+ response.body().string());*/
 
-        return new FcmResponse(
+        /*return new FcmResponse(
                 title,
                 body,
                 "LikeFirst_BTOS",
-                "LikeFirst_BTOS");
+                "LikeFirst_BTOS");*/
     }
 
     private String makeMessage(String targetToken, String title, String body) throws JsonProcessingException {
