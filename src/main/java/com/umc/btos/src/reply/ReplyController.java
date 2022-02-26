9c214c1f3181ce76d017a97a96c853bbb487e3ff
euna <christinejung10@daum.net>
2022-02-26 16:11:47 +0900

    Update for prod

--- src/main/java/com/umc/btos/src/reply/ReplyController.java
+++ src/main/java/com/umc/btos/src/reply/ReplyController.java
@@ -1,84 +1,55 @@
-package com.umc.btos.src.reply;
-
-import com.umc.btos.config.BaseException;
-import com.umc.btos.config.BaseResponse;
-import com.umc.btos.src.reply.model.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import static com.umc.btos.config.BaseResponseStatus.*;
-
-@RestController
-@RequestMapping("/replies")
-public class ReplyController {
-    final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
-    private final ReplyProvider replyProvider;
-    @Autowired
-    private final ReplyService replyService;
-
-    public ReplyController(ReplyProvider replyProvider, ReplyService replyService) {
-        this.replyProvider = replyProvider;
-        this.replyService = replyService;
-    }
-
-    /*
-     * 답장 저장 및 발송
-     * [POST] /replies
-     */
-    @ResponseBody
-    @PostMapping("")
-    public BaseResponse<String> postReply(@RequestBody PostReplyReq postReplyReq) {
-        try {
-            int replyIdx = replyService.postReply(postReplyReq);
-
-            String result = "답장 저장 및 발송이 완료되었습니다. (replyIdx = " + replyIdx + ")";
-            return new BaseResponse<>(result);
-
-        } catch (BaseException exception) {
-            return new BaseResponse<>((exception.getStatus()));
-        }
-    }
-
-    /*
-     * 답장 삭제
-     * [PATCH] /replies/:replyIdx?userIdx=
-     */
-    @ResponseBody
-    @PatchMapping("/{replyIdx}")
-    public BaseResponse<String> deleteReply(@PathVariable("replyIdx") int replyIdx, @RequestParam("userIdx") int userIdx) {
-        try {
-            // TODO : 형식적 validation - 존재하는 회원인가? / 존재하는 답장인가? / 해당 회원이 작성한 답장인가?
-            if (replyProvider.checkUserIdx(userIdx) == 0) {
-                throw new BaseException(INVALID_USERIDX); // 존재하지 않는 회원입니다.
-            }
-            if (replyProvider.checkReplyIdx(replyIdx) == 0) {
-                throw new BaseException(INVALID_LETTERIDX); // 존재하지 않는 답장입니다.
-            }
-            if (replyProvider.checkUserAboutReply(userIdx, replyIdx) == 0) {
-                throw new BaseException(INVALID_USER_ABOUT_REPLY); // 해당 답장에 접근 권한이 없는 회원입니다.
-            }
-
-            replyService.deleteReply(replyIdx);
-            String result = "답장(replyIdx = " + replyIdx + ")이 삭제되었습니다.";
-            return new BaseResponse<>(result);
-
-        } catch (BaseException exception) {
-            return new BaseResponse<>((exception.getStatus()));
-        }
-    }
-
-    /*
-     * 답장 조회
-     * [GET] /replies/:replyIdx?userIdx=
-     * 답장 열람 여부 변경 (Reply.isChecked : 0 -> 1)
-     */
+//package com.umc.btos.src.reply;
+//
+//import com.umc.btos.config.BaseException;
+//import com.umc.btos.config.BaseResponse;
+//import com.umc.btos.src.reply.model.*;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import static com.umc.btos.config.BaseResponseStatus.*;
+//
+//@RestController
+//@RequestMapping("/replies")
+//public class ReplyController {
+//    final Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//    @Autowired
+//    private final ReplyProvider replyProvider;
+//    @Autowired
+//    private final ReplyService replyService;
+//
+//    public ReplyController(ReplyProvider replyProvider, ReplyService replyService) {
+//        this.replyProvider = replyProvider;
+//        this.replyService = replyService;
+//    }
+//
+//    /*
+//     * 답장 저장 및 발송
+//     * [POST] /replies
+//     */
 //    @ResponseBody
-//    @GetMapping("/{replyIdx}")
-//    public BaseResponse<GetReplyRes> getReply(@PathVariable("replyIdx") int replyIdx, @RequestParam("userIdx") int userIdx) {
+//    @PostMapping("")
+//    public BaseResponse<String> postReply(@RequestBody PostReplyReq postReplyReq) {
+//        try {
+//            int replyIdx = replyService.postReply(postReplyReq);
+//
+//            String result = "답장 저장 및 발송이 완료되었습니다. (replyIdx = " + replyIdx + ")";
+//            return new BaseResponse<>(result);
+//
+//        } catch (BaseException exception) {
+//            return new BaseResponse<>((exception.getStatus()));
+//        }
+//    }
+//
+//    /*
+//     * 답장 삭제
+//     * [PATCH] /replies/:replyIdx?userIdx=
+//     */
+//    @ResponseBody
+//    @PatchMapping("/{replyIdx}")
+//    public BaseResponse<String> deleteReply(@PathVariable("replyIdx") int replyIdx, @RequestParam("userIdx") int userIdx) {
 //        try {
 //            // TODO : 형식적 validation - 존재하는 회원인가? / 존재하는 답장인가? / 해당 회원이 작성한 답장인가?
 //            if (replyProvider.checkUserIdx(userIdx) == 0) {
@@ -91,12 +62,41 @@ public class ReplyController {
 //                throw new BaseException(INVALID_USER_ABOUT_REPLY); // 해당 답장에 접근 권한이 없는 회원입니다.
 //            }
 //
-//            GetReplyRes getReplyRes = replyProvider.getReply(replyIdx);
-//            return new BaseResponse<>(getReplyRes);
+//            replyService.deleteReply(replyIdx);
+//            String result = "답장(replyIdx = " + replyIdx + ")이 삭제되었습니다.";
+//            return new BaseResponse<>(result);
 //
 //        } catch (BaseException exception) {
 //            return new BaseResponse<>((exception.getStatus()));
 //        }
 //    }
-
-}
+//
+//    /*
+//     * 답장 조회
+//     * [GET] /replies/:replyIdx?userIdx=
+//     * 답장 열람 여부 변경 (Reply.isChecked : 0 -> 1)
+//     */
+////    @ResponseBody
+////    @GetMapping("/{replyIdx}")
+////    public BaseResponse<GetReplyRes> getReply(@PathVariable("replyIdx") int replyIdx, @RequestParam("userIdx") int userIdx) {
+////        try {
+////            // TODO : 형식적 validation - 존재하는 회원인가? / 존재하는 답장인가? / 해당 회원이 작성한 답장인가?
+////            if (replyProvider.checkUserIdx(userIdx) == 0) {
+////                throw new BaseException(INVALID_USERIDX); // 존재하지 않는 회원입니다.
+////            }
+////            if (replyProvider.checkReplyIdx(replyIdx) == 0) {
+////                throw new BaseException(INVALID_LETTERIDX); // 존재하지 않는 답장입니다.
+////            }
+////            if (replyProvider.checkUserAboutReply(userIdx, replyIdx) == 0) {
+////                throw new BaseException(INVALID_USER_ABOUT_REPLY); // 해당 답장에 접근 권한이 없는 회원입니다.
+////            }
+////
+////            GetReplyRes getReplyRes = replyProvider.getReply(replyIdx);
+////            return new BaseResponse<>(getReplyRes);
+////
+////        } catch (BaseException exception) {
+////            return new BaseResponse<>((exception.getStatus()));
+////        }
+////    }
+//
+//}
--- src/main/java/com/umc/btos/src/suggest/SuggestController.java
+++ src/main/java/com/umc/btos/src/suggest/SuggestController.java
@@ -1,46 +1,62 @@
-package com.umc.btos.src.suggest;
-
-import com.umc.btos.config.BaseException;
-import com.umc.btos.config.BaseResponse;
-import com.umc.btos.src.suggest.model.PostSuggestReq;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import static com.umc.btos.config.BaseResponseStatus.*;
-
-@RestController
-@RequestMapping("/suggests")
-public class SuggestController {
-    final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
-    private final SuggestProvider suggestProvider;
-    @Autowired
-    private final SuggestService suggestService;
-
-    public SuggestController(SuggestProvider suggestProvider, SuggestService suggestService) {
-        this.suggestProvider = suggestProvider;
-        this.suggestService = suggestService;
-    }
-
-    /*
-     * 건의 저장
-     * [POST] /suggests
-     */
-    @ResponseBody
-    @PostMapping("")
-    public BaseResponse<String> postSuggest(@RequestBody PostSuggestReq postSuggestReq) {
-        try {
-            int suggestIdx = suggestService.postSuggest(postSuggestReq);
-
-            String result = "건의 저장 완료 (suggestIdx = " + suggestIdx + ")";
-            return new BaseResponse<>(result);
-
-        } catch (BaseException exception) {
-            return new BaseResponse<>(exception.getStatus());
-        }
-    }
-
-}
+//package com.umc.btos.src.suggest;
+//
+//import com.umc.btos.config.BaseException;
+//import com.umc.btos.config.BaseResponse;
+//import com.umc.btos.src.suggest.model.PostSuggestReq;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import static com.umc.btos.config.BaseResponseStatus.*;
+//
+//// SQL
+///*
+//create table Suggest
+//        (
+//        suggestIdx int auto_increment
+//        primary key,
+//        userIdx    int                                   not null comment '회원 식별자',
+//        type       varchar(10)                           not null comment '건의 구분 (bug : 버그 제보 / user : 악성 유저 신고 / add : 추가되었으면 하는 기능 / etc : 기타)',
+//        content    varchar(200)                          not null comment '내용',
+//        status     varchar(10) default 'active'          not null comment '상태값 (active / deleted)',
+//        createdAt  timestamp   default CURRENT_TIMESTAMP not null comment '생성일 (건의 작성일)',
+//        updatedAt  timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '수정일'
+//        )
+//        comment '건의';
+//*/
+//
+//@RestController
+//@RequestMapping("/suggests")
+//public class SuggestController {
+//    final Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//    @Autowired
+//    private final SuggestProvider suggestProvider;
+//    @Autowired
+//    private final SuggestService suggestService;
+//
+//    public SuggestController(SuggestProvider suggestProvider, SuggestService suggestService) {
+//        this.suggestProvider = suggestProvider;
+//        this.suggestService = suggestService;
+//    }
+//
+//    /*
+//     * 건의 저장
+//     * [POST] /suggests
+//     */
+//    @ResponseBody
+//    @PostMapping("")
+//    public BaseResponse<String> postSuggest(@RequestBody PostSuggestReq postSuggestReq) {
+//        try {
+//            int suggestIdx = suggestService.postSuggest(postSuggestReq);
+//
+//            String result = "건의 저장 완료 (suggestIdx = " + suggestIdx + ")";
+//            return new BaseResponse<>(result);
+//
+//        } catch (BaseException exception) {
+//            return new BaseResponse<>(exception.getStatus());
+//        }
+//    }
+//
+//}
